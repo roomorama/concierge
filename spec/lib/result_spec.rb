606e8821ee72796a6b619d6a1f989ff7a38e46c8
Renato Mascarenhas <renato@roomorama.com>
2016-03-01 19:14:21 +0800

    Add implementation of a result object.

---
+++ spec/lib/result_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+RSpec.describe Result do
+  let(:result) { 42 }
+
+  subject { described_class.new(result) }
+
+  describe "#error" do
+    it "is able to store an error associated with an operation" do
+      subject.error.code = :invalid_operation
+      subject.error.message = "Tried to perform invalid operation"
+
+      expect(subject.error.code).to eq :invalid_operation
+      expect(subject.error.message).to eq "Tried to perform invalid operation"
+    end
+  end
+
+  describe "#success?" do
+    it "is successful if there is no error associated with the result" do
+      expect(subject).to be_success
+    end
+
+    it "is not successful if there is an error associated with the result" do
+      subject.error.code = :invalid_operation
+      expect(subject).not_to be_success
+    end
+  end
+
+  describe "#value" do
+    it "returns the wrapped result if successful" do
+      expect(subject.value).to eq 42
+    end
+
+    it "is nil if the operation was not successful" do
+      subject.error.code = :invalid_operation
+      expect(subject.value).to be_nil
+    end
+  end
+end
