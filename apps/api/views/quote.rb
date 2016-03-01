93aa1b630d318ae7c663ddf0a3414e6d92006f2a
Renato Mascarenhas <renato@roomorama.com>
2016-03-01 14:34:31 +0800

    Remove unnecessary logic.
    Now that the 503 logic was moved to the controller, the view will only
    be rendered on successful scenarios.

--- apps/api/views/quote.rb
+++ apps/api/views/quote.rb
@@ -5,30 +5,14 @@ module API::Views
     layout false
 
     def render
-      if quotation.successful?
-        response = quotation_attributes
-      else
-        response = errors
-      end
-
-      json(response)
-    end
-
-    private
-
-    def errors
-      { status: "error" }.merge!(errors: quotation.errors)
-    end
-
-    def quotation_attributes
-      {
+      json({
         status:     "ok",
         check_in:   quotation.check_in,
         check_out:  quotation.check_out,
         guests:     quotation.guests,
         currency:   quotation.currency,
         total:      quotation.total
-      }
+      })
     end
 
   end
