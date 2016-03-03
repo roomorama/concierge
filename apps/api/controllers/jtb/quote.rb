cf6ae13657d014736c7814cfac0714a4f1cbed99
igor <igortr333@gmail.com>
2016-03-03 13:23:15 +1000

    copy paste

---
+++ apps/api/controllers/jtb/quote.rb
@@ -0,0 +1,16 @@
+require_relative "../quote"
+
+module API::Controllers::Jtb
+
+  # API::Controllers::Jtb::Quote
+  #
+  # Performs booking quotations for properties from Jtb.
+  class Quote
+    include API::Controllers::Quote
+
+    def quote_price(params)
+      Jtb::Client.new.quote(params)
+    end
+
+  end
+end
