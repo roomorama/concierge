fd095fa96ca0589d503dfad00ec1f4f32cc65283
Renato Mascarenhas <renato@roomorama.com>
2016-03-03 12:11:12 +0800

    Adds a Quotation attribute to determine if the property is available.

--- lib/concierge/entities/quotation.rb
+++ lib/concierge/entities/quotation.rb
@@ -9,6 +9,7 @@
 #   +check_in+:    the check-in date for the stay
 #   +check_out+:   the check-out date for the stay
 #   +guests+:      the number of guests
+#   +available+:   whether or not the property is available for the given dates
 #   +total+:       the quoted price for the booking
 #   +currency+:    the currency used for the quotation
 #   +errors+:      if there were any errors during the quotation with the partner,
@@ -23,6 +24,7 @@ class Quotation
   attribute :check_in,    type: Date
   attribute :check_out,   type: Date
   attribute :guests,      type: Integer
+  attribute :available,   type: Boolean
   attribute :total,       type: Integer
   attribute :currency,    type: String
   attribute :errors,      type: Hash
