3ea814375183860b80d718fc826e475811750c99
Renato Mascarenhas <renato@roomorama.com>
2016-03-07 18:23:36 +0800

    Add legacy endpoint.

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,3 +1,4 @@
-get '/atleisure/quote', to: 'at_leisure#quote'
-get '/jtb/quote',       to: 'jtb#quote'
-get '/kigo/quote',      to: 'kigo#quote'
+get '/atleisure/quote',   to: 'at_leisure#quote'
+get '/jtb/quote',         to: 'jtb#quote'
+get '/kigo/quote',        to: 'kigo#quote'
+get '/kigo/legacy/quote', to: 'kigo/legacy#quote'
