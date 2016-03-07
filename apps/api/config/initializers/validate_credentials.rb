f08e26203eb7e579daf690007eb9264181213da1
Renato Mascarenhas <renato@roomorama.com>
2016-03-07 19:01:47 +0800

    Properly configure credentials for Kigo.

--- apps/api/config/initializers/validate_credentials.rb
+++ apps/api/config/initializers/validate_credentials.rb
@@ -1,6 +1,8 @@
 if Hanami.env == "production"
   Credentials.validate_credentials!({
-    atleisure: %w(username password),
-    jtb: %w(id user password company)
+    atleisure:  %w(username password),
+    jtb:        %w(id user password company),
+    kigo:       %w(subscription_key),
+    kigolegacy: %w(username password)
   })
 end
