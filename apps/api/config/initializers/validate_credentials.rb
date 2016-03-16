b94f4717a46f676550dc0e1d535feb507cf88d07
Renato Mascarenhas <renato@roomorama.com>
2016-03-16 15:02:16 +0800

    Make sure Roomorama's webhook secret is set on startup.

--- apps/api/config/initializers/validate_credentials.rb
+++ apps/api/config/initializers/validate_credentials.rb
@@ -1,8 +1,9 @@
 if Hanami.env == "production"
   Concierge::Credentials.validate_credentials!({
-    atleisure:  %w(username password),
-    jtb:        %w(id user password company url),
-    kigo:       %w(subscription_key),
-    kigolegacy: %w(username password)
+    atleisure:          %w(username password),
+    jtb:                %w(id user password company url),
+    kigo:               %w(subscription_key),
+    kigolegacy:         %w(username password)
+    roomorama_webhooks: %w(secret)
   })
 end
