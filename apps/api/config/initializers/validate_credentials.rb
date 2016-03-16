2210b090c11b276a2aab9bebbd9cb5e39a00216e
Renato Mascarenhas <renato@roomorama.com>
2016-03-16 15:03:09 +0800

    Make sure all env vars are defined on staging as well.

--- apps/api/config/initializers/validate_credentials.rb
+++ apps/api/config/initializers/validate_credentials.rb
@@ -1,9 +1,11 @@
-if Hanami.env == "production"
+enforce_on_envs = ["staging", "production"]
+
+if enforce_on_envs.include?(Hanami.env)
   Concierge::Credentials.validate_credentials!({
     atleisure:          %w(username password),
     jtb:                %w(id user password company url),
     kigo:               %w(subscription_key),
-    kigolegacy:         %w(username password)
+    kigolegacy:         %w(username password),
     roomorama_webhooks: %w(secret)
   })
 end
