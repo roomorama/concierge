7884981ecfb4da22f7dca9206adbd4c5f91826fe
Renato Mascarenhas <renato@roomorama.com>
2016-03-16 15:21:09 +0800

    Make sure all requests are POST requests.
    This is how Roomorama's webhooks work at the moment.

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -1,7 +1,7 @@
-get '/atleisure/quote',   to: 'at_leisure#quote'
-get '/jtb/quote',         to: 'j_t_b#quote'
-get '/kigo/quote',        to: 'kigo#quote'
-get '/kigo/legacy/quote', to: 'kigo/legacy#quote'
-get '/poplidays/quote',   to: 'poplidays#quote'
+post '/atleisure/quote',   to: 'at_leisure#quote'
+post '/jtb/quote',         to: 'j_t_b#quote'
+post '/kigo/quote',        to: 'kigo#quote'
+post '/kigo/legacy/quote', to: 'kigo/legacy#quote'
+post '/poplidays/quote',   to: 'poplidays#quote'
 
 post '/jtb/booking',      to: 'j_t_b#booking'
