29a38f3ad31ae1ffc18c2917ba9fd72016c94526
Renato Mascarenhas <renato@roomorama.com>
2016-03-22 13:55:14 +0800

    Support for Rollbar reporting.

---
+++ apps/api/config/initializers/rollbar.rb
@@ -0,0 +1,6 @@
+Rollbar.configure do |config|
+  config.access_token = ENV["ROLLBAR_ACCESS_TOKEN"]
+  config.environment  = Hanami.env
+  config.framework    = "Hanami #{Hanami::VERSION}"
+  config.root         = Hanami.root.to_s
+end
