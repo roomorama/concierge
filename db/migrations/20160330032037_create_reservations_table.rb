c276b8bf5484c1693b6dd6e3de9938022c04368b
igor <igortr333@gmail.com>
2016-03-30 15:17:41 +1000

    align columns

--- db/migrations/20160330032037_create_reservations_table.rb
+++ db/migrations/20160330032037_create_reservations_table.rb
@@ -3,12 +3,12 @@ Hanami::Model.migration do
     create_table :reservations do
       primary_key :id
 
-      column :property_id, String, null: false
-      column :unit_id, String
-      column :check_in, Date, null: false
-      column :check_out, Date, null: false
-      column :guests, Integer, null: false
-      column :code, String, null: false
+      column :property_id, String,  null: false
+      column :unit_id,     String
+      column :check_in,    Date,    null: false
+      column :check_out,   Date,    null: false
+      column :guests,      Integer, null: false
+      column :code,        String,  null: false
     end
   end
 end
