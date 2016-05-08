378300a5b618bda4688aac204c9f2da135e10c8b
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2016-05-08 13:01:38 +0800

    Better error message, fix env var name.

--- config/environment.rb
+++ config/environment.rb
@@ -1,9 +1,20 @@
-require 'rubygems'
-require 'bundler/setup'
-require 'hanami/setup'
-require_relative '../lib/concierge'
-require_relative '../apps/api/application'
+require "rubygems"
+require "bundler/setup"
+require "hanami/setup"
+require_relative "../lib/concierge"
+
+apps = %w(api web)
+unless apps.include?(Concierge.app)
+  raise RuntimeError.new("Unknown CONCIERGE_APP: #{Concierge.app}")
+end
+
+require_relative "../apps/#{Concierge.app}/application"
 
 Hanami::Container.configure do
-  mount API::Application, at: '/'
+  case Concierge.app
+  when "api"
+    mount API::Application, at: "/"
+  when "web"
+    mount Web::Application, at: "/"
+  end
 end
