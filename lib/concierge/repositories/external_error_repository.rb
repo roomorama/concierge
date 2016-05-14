241a46efd48c15c57a4576b16f60b51f1bb8606c
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2016-05-14 19:11:11 +0800

    Paginate collection of external errors.

--- lib/concierge/repositories/external_error_repository.rb
+++ lib/concierge/repositories/external_error_repository.rb
@@ -4,6 +4,8 @@
 class ExternalErrorRepository
   include Hanami::Repository
 
+  DEFAULT_PER_PAGE = 10
+
   # returns the total number of external errors stored in the database.
   def self.count
     query.count
@@ -14,5 +16,20 @@ class ExternalErrorRepository
   def self.most_recent
     scope = query { order(:happened_at).limit(1) }
     scope.to_a.first
+  end
+
+  # paginates the collection of external errors according to the parameters given.
+  #
+  # page - the page to be returned.
+  # per  - the number of results per page.
+  #
+  # If +page+ is not given, this method will return the first page. If +per+
+  # is not given, this method will assume 10 results per page.
+  def self.paginate(page: nil, per: nil)
+    page ||= 1
+    per  ||= DEFAULT_PER_PAGE
+
+    offset = (page - 1) * per
+    query { desc(:happened_at).offset(offset).limit(per) }
   end
 end
--- spec/lib/concierge/repositories/external_error_repository_spec.rb
+++ spec/lib/concierge/repositories/external_error_repository_spec.rb
@@ -37,6 +37,30 @@ RSpec.describe ExternalErrorRepository do
     end
   end
 
+  describe ".paginate" do
+    before do
+      create_error(happened_at: Time.now - 24 * 60 * 60)
+      create_error(supplier: "SupplierB")
+    end
+
+    it "uses the defaults in case the parameters given are nil" do
+      collection = described_class.paginate.to_a
+
+      expect(collection.size).to eq 2
+      expect(collection.first.supplier).to eq "SupplierB"
+      expect(collection.last.supplier).to eq "SupplierA"
+    end
+
+    it "uses the parameters given" do
+      collection = described_class.paginate(per: 1).to_a
+      expect(collection.size).to eq 1
+      expect(collection.first.supplier).to eq "SupplierB"
+
+      collection = described_class.paginate(page: 2).to_a
+      expect(collection.size).to eq 0
+    end
+  end
+
   private
 
   def create_error(overrides = {})
