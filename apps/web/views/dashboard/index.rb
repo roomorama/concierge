5b311b62ed36290c622d53309d324640f2e83035
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2016-05-14 17:22:51 +0800

    Specs and documentation for the dashboard page.

--- apps/web/views/dashboard/index.rb
+++ apps/web/views/dashboard/index.rb
@@ -1,4 +1,14 @@
 module Web::Views::Dashboard
+
+  # +Web::Views::Dashboard::Index+
+  #
+  # This view is presented on Concierge's dashboard page.
+  # It includes information on the status of the API.
+  #
+  # This view expects an instance of +Web::Support::StatusCheck+
+  # for more information on API status check being exposed as
+  # +concierge+. Check the documentation of that class for further
+  # information on API status check.
   class Index
     include Web::View
 
@@ -8,6 +18,15 @@ module Web::Views::Dashboard
 
     def concierge_healthy
       label(success: "Yes", failure: "No") { concierge.healthy? }
+    end
+
+    # +concierge.response+ is a +Result+ instance that wraps a
+    # +Faraday::Response+ instance.
+    def error_message
+      unless concierge.response.success?
+        error = concierge.response.error
+        html.code [error.code, " - ", error.message].join
+      end
     end
 
     private
--- spec/web/views/dashboard/index_spec.rb
+++ spec/web/views/dashboard/index_spec.rb
@@ -1,12 +1,54 @@
-require_relative '../../../../apps/web/views/dashboard/index'
+require "spec_helper"
 
 RSpec.describe Web::Views::Dashboard::Index do
-  let(:exposures) { Hash[foo: 'bar'] }
-  let(:template)  { Hanami::View::Template.new('apps/web/templates/dashboard/index.html.erb') }
-  let(:view)      { described_class.new(template, exposures) }
-  let(:rendered)  { view.render }
+  include Support::HTTPStubbing
 
-  it 'exposes #foo' do
-    expect(view.foo).to eq exposures.fetch(:foo)
+  let(:status_check) { Web::Support::StatusCheck.new }
+  let(:exposures)    { Hash[concierge: status_check] }
+  let(:template)     { Hanami::View::Template.new('apps/web/templates/dashboard/index.html.erb') }
+  let(:view)         { described_class.new(template, exposures) }
+  let(:rendered)     { view.render }
+
+  let(:application_json) { { "Content-Type" => "application/json" } }
+  let(:successful_response) {
+    {
+      "status"  => "ok",
+      "time"    => "2016-05-14 05:58:56 UTC",
+      "version" => "0.1.4"
+    }.to_json
+  }
+
+  def concierge_responds_with
+    stub_call(:get, "https://concierge.roomorama.com/_ping") { yield }
+  end
+
+  it "indicates that Concierge is alive" do
+    concierge_responds_with { [200, application_json, successful_response] }
+    expect(rendered).to include %(<span class="concierge-success">Up</span>)
+  end
+
+  it "indicates that Concierge is unreachable" do
+    concierge_responds_with { raise Faraday::TimeoutError }
+    expect(rendered).to include %(<span class="concierge-failure">Down</span>)
+  end
+
+  it "indicates that Concierge is healthy" do
+    concierge_responds_with { [200, application_json, successful_response] }
+    expect(rendered).to include %(<span class="concierge-success">Yes</span>)
+  end
+
+  it "indicates that Concierge is not healthy" do
+    concierge_responds_with { [500, application_json, successful_response] }
+    expect(rendered).to include %(<span class="concierge-failure">No</span>)
+  end
+
+  it "shows Concierge running version" do
+    concierge_responds_with { [200, application_json, successful_response] }
+    expect(rendered).to include %(<strong>Version:</strong> 0.1.4)
+  end
+
+  it "shows the error message in case Concierge is not healthy" do
+    concierge_responds_with { [500, application_json, "Something went wrong"] }
+    expect(rendered).to include %(<strong>Error:</strong> <code>http_status_500 - Something went wrong</code>)
   end
 end
