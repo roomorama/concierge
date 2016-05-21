7b05867be06973e2bae438ef1efdd08a51675b08
Renato Mascarenhas <renato@roomorama.com>
2016-05-21 10:44:52 +0800

    Make sure initializers are run consistently across FS.

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ unless apps.keys.include?(Concierge.app)
   raise RuntimeError.new("Unknown CONCIERGE_APP: #{Concierge.app}")
 end
 
-Dir["./config/initializers/*.rb"].each { |f| require f }
+Dir["./config/initializers/*.rb"].sort.each { |f| require f }
 
 apps[Concierge.app].each { |app| require_relative "../apps/#{app}/application" }
 
