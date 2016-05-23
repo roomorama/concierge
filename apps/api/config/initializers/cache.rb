710aec8d728b11971a12165a1dbec514369aa6c8
Renato Mascarenhas <renato@roomorama.com>
2016-05-23 11:36:10 +0800

    Subscribes and reports cache hit/miss events.

---
+++ apps/api/config/initializers/cache.rb
@@ -0,0 +1,22 @@
+# subscribes to +Concierge::Announcer+ events published by the
+# +Concierge::Cache+ class. See that class documentation,
+# as well that of the classes under +Concierge::Context+ to understand
+# the rationale.
+
+Concierge::Announcer.on(Concierge::Cache::CACHE_HIT) do |key, value, content_type|
+  cache_hit = Concierge::Context::CacheHit.new(
+    key:          key,
+    value:        value,
+    content_type: content_type
+  )
+
+  API.context.augment(cache_hit)
+end
+
+Concierge::Announcer.on(Concierge::Cache::CACHE_MISS) do |key|
+  cache_miss = Concierge::Context::CacheMiss.new(
+    key: key
+  )
+
+  API.context.augment(cache_miss)
+end
