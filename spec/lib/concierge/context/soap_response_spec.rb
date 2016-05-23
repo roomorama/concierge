2fe75fad6c4083afe77b176fe585c8ff372b22b4
Renato Mascarenhas <renato@roomorama.com>
2016-05-23 10:46:40 +0800

    SOAPResponse event.

---
+++ spec/lib/concierge/context/soap_response_spec.rb
@@ -0,0 +1,33 @@
+require "spec_helper"
+
+RSpec.describe Concierge::Context::SOAPResponse do
+  let(:params) {
+    {
+      status:       "200",
+      headers: {
+        "Connection"   => "keep-alive",
+        "Content-Type" => "application/xml"
+      },
+      body: "<status>OK</status>"
+    }
+  }
+
+  subject { described_class.new(params) }
+
+  describe "#to_h" do
+    it "serializes the information to a valid hash" do
+      allow(Time).to receive(:now) { Time.new("2016", "05", "21", "16", "15", "42") }
+
+      expect(subject.to_h).to eq({
+        type:      "soap_response",
+        timestamp: Time.now,
+        status:    "200",
+        headers: {
+          "Connection"   => "keep-alive",
+          "Content-Type" => "application/xml"
+        },
+        body: "<status>OK</status>"
+      })
+    end
+  end
+end
