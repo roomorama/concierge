18649a7fe368ced8934e7d89d500484c56a82fe7
Renato Mascarenhas <renato@roomorama.com>
2016-05-23 15:37:26 +0800

    Write a client to perform arbitrary operations.

---
+++ lib/concierge/roomorama/client/operations.rb
@@ -0,0 +1,16 @@
+class Roomorama::Client
+
+  # +Roomorama::Client::Operations+
+  #
+  # Provides a set of shortcut methods for performing operations on the
+  # Roomorama API.
+  class Operations
+
+    # Performs a +publish+ operation for the given property.
+    def self.publish(property)
+      Publish.new(property)
+    end
+
+  end
+
+end
