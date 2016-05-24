8941b3889ac99e4775a0f0689240b07be6baabeb
Renato Mascarenhas <renato@roomorama.com>
2016-05-24 18:34:53 +0800

    Introduce workers app.

---
+++ apps/workers/application.rb
@@ -0,0 +1,33 @@
+module Workers
+  class Application < Hanami::Application
+    configure do
+      root __dir__
+
+      load_paths << [
+        "suppliers"
+      ]
+
+      cookies false
+      layout false
+    end
+
+    configure :development do
+      handle_exceptions false
+    end
+
+    configure :test do
+      handle_exceptions false
+    end
+
+    configure :staging do
+      handle_exceptions false
+    end
+
+    configure :production do
+      # this is a worker app, comprised of background processors. There is no need
+      # handle exceptions, since we want them to be raised so that we
+      # get notified.
+      handle_exceptions false
+    end
+  end
+end
