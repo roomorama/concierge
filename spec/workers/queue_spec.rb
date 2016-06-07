8138c8e43ac65dbe89317c1b86f30e035828a5f0
Renato Mascarenhas <renato@roomorama.com>
2016-06-07 13:54:02 +0800

    Improve queue polling, with error detection.

--- spec/workers/queue_spec.rb
+++ spec/workers/queue_spec.rb
@@ -42,5 +42,53 @@ RSpec.describe Workers::Queue do
 
       subject.poll
     end
+
+    context "message processing" do
+      class TestPoller
+        attr_reader :message
+
+        def initialize(message)
+          @message = message
+        end
+
+        def poll(*)
+          yield(message)
+        end
+
+        def delete_message(*)
+        end
+      end
+
+      let(:message) { "message" }
+      let(:poller)  { TestPoller.new(message) }
+
+      before do
+        allow(subject).to receive(:queue_poller) { poller }
+      end
+
+      it "ensures that the result from the message processing block is a valid Result instance" do
+        expect {
+          subject.poll do
+            42
+          end
+        }.to raise_error Workers::Queue::InvalidQueueProcessingResultError
+      end
+
+      it "does not delete the message if the message processing indicates failure" do
+        expect(poller).not_to receive(:delete_message).with(message)
+
+        subject.poll do
+          Result.error(:something_went_wrong)
+        end
+      end
+
+      it "deletes the message if the message processing indicates success" do
+        expect(poller).to receive(:delete_message).with(message)
+
+        subject.poll do
+          Result.new(42)
+        end
+      end
+    end
   end
 end
