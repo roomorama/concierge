854c9f504af087d5303ec80196c5ac9ba63cccd4
Renato Mascarenhas <renato@roomorama.com>
2016-06-07 15:08:00 +0800

    Time out all queue message processing.

--- spec/workers/processor_spec.rb
+++ spec/workers/processor_spec.rb
@@ -44,5 +44,21 @@ RSpec.describe Workers::Processor do
       expect(result).to be_success
       expect(invoked).to eq true
     end
+
+    it "times out and fails if the operation takes too long" do
+      supplier = create_supplier(name: "AcmeTest")
+      host     = create_host(username: "acme-host", supplier_id: supplier.id)
+      payload[:data][:host_id] = host.id
+
+      # simulates a timeout of 0.5s and a synchronisation process that takes
+      # one minute, thus timing out.
+      allow(subject).to receive(:processing_timeout) { 0.5 }
+      Concierge::Announcer.on("sync.AcmeTest") { sleep 1 }
+
+      result = subject.process!
+      expect(result).to be_a Result
+      expect(result).not_to be_success
+      expect(result.error.code).to eq :timeout
+    end
   end
 end
