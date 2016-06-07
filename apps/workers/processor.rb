07be3292e2ceb7e735c89138ad44e7cee803aca1
Renato Mascarenhas <renato@roomorama.com>
2016-06-07 15:08:00 +0800

    Time out all queue message processing.

--- apps/workers/processor.rb
+++ apps/workers/processor.rb
@@ -1,3 +1,5 @@
+require "timeout"
+
 module Workers
 
   # +Workers::Processor+
@@ -30,6 +32,12 @@ module Workers
       end
     end
 
+    class TimeoutError < StandardError
+      def initialize(operation, params)
+        super("Processing of operation #{operation}, with args #{params} timed out.")
+      end
+    end
+
     include Concierge::JSON
 
     attr_reader :payload
@@ -59,16 +67,36 @@ module Workers
     private
 
     def perform_sync(args)
-      host      = HostRepository.find(args[:host_id])
-      supplier  = SupplierRepository.find(host.supplier_id)
-      broadcast = ["sync", ".", supplier.name].join
+      timing_out("sync", args) do
+        host      = HostRepository.find(args[:host_id])
+        supplier  = SupplierRepository.find(host.supplier_id)
+        broadcast = ["sync", ".", supplier.name].join
 
-      Concierge::Announcer.trigger(broadcast, host)
-      Result.new(true)
+        Concierge::Announcer.trigger(broadcast, host)
+        Result.new(true)
+      end
+    end
+
+    # timing out the operation to be processed by the queue makes sure that
+    # jobs take too long are not taken by a different worker, possibly duplicating
+    # work and causing conflicts.
+    def timing_out(operation, params)
+      Timeout.timeout(processing_timeout) { yield }
+    rescue Timeout::Error
+      error = TimeoutError.new(operation, params)
+      Rollbar.error(error)
+
+      Result.error(:timeout)
     end
 
     def message
       @message = json_decode(payload)
+    end
+
+    # NOTE this time out should be shorter than the +VisibilityTimeout+ configured
+    # on the SQS queue to be used by Concierge.
+    def processing_timeout
+      @one_hour ||= 60 * 60
     end
   end
 
