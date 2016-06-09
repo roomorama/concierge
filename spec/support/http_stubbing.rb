3cafe99aab9ac49e9c0e4238bd188a65bfb8f851
Igor Trofimov <igor@roomorama.com>
2016-06-09 10:54:52 +0800

    fix indentation

--- spec/support/http_stubbing.rb
+++ spec/support/http_stubbing.rb
@@ -23,9 +23,9 @@ module Support
     #
     # The block passed is expected to return a Rack-style response.
     def stub_call(http_method, url, options = {})
-      uri = URI(url)
+      uri      = URI(url)
       endpoint = [uri.scheme, "://", uri.host].join
-      path = uri.path
+      path     = uri.path
 
       stubs.public_send(http_method, path) { yield }
 
@@ -42,11 +42,12 @@ module Support
     end
 
     ResponseWrapper = Struct.new(:status, :headers, :body)
+
     def parse_response(rack_response)
       ResponseWrapper.new(
-          rack_response[0],
-          rack_response[1],
-          JSON.parse(rack_response[2].first)
+        rack_response[0],
+        rack_response[1],
+        JSON.parse(rack_response[2].first)
       )
     end
   end
