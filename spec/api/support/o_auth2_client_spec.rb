201dee3651dbca430586c91ef1977d2d22126b5d
keang <skeang@gmail.com>
2016-06-09 10:08:26 +0800

    Add oauth2 get and post And happy specs

--- spec/api/support/o_auth2_client_spec.rb
+++ spec/api/support/o_auth2_client_spec.rb
@@ -5,22 +5,68 @@ RSpec.describe API::Support::OAuth2Client do
   include Support::Fixtures
   include Support::HTTPStubbing
 
-  let(:base_url) { "https://apis.waytostay.com" }
-  let(:token_url) { "/oauth" }
-  let(:client) { described_class.new({id: "id",
-                                      secret: "secret",
-                                      token_url: token_url,
-                                      base_url: base_url}) }
+  let(:credentials) { Concierge::Credentials.for("waytostay") }
+  let(:client) { described_class.new({id: credentials[:client_id],
+                                      secret: credentials[:client_secret],
+                                      token_url: credentials[:token_url],
+                                      base_url: credentials[:url]}) }
+
+  before do
+    client.oauth_client.connection = stub_call( :post,
+                                               credentials[:url] + credentials[:token_url] ) {
+      [200, {'Content-Type'=>'application/json'},
+       read_fixture("waytostay#{credentials[:token_url]}.json")]
+    }
+  end
 
   describe "#access_token" do
-    before do
-      client.oauth_client.connection = stub_call(:post, base_url + token_url,) {
-        [200, {'Content-Type'=>'application/json'}, read_fixture("waytostay/oauth.json")]
-      }
+    context "when successful" do
+
+      subject { client.access_token }
+
+      it { expect(subject).to be_a(OAuth2::AccessToken) }
+      it { expect(subject.token).to be_a(String) }
+    end
+  end
+
+  describe "#get" do
+
+    let(:endpoint) { "/ping" }
+
+    subject { client.get(endpoint) }
+
+    context "when successful" do
+      before do
+        client.oauth_client.connection = stub_call(:get,
+                                                   credentials[:url] + endpoint ) {
+          [200, {'Content-Type'=>'application/json'},
+           read_fixture("waytostay#{endpoint}.json")]
+        }
+      end
+
+      it { expect(subject.value).to be_a(Result) }
+      it { expect(subject).to be_success }
     end
 
-    subject { client.access_token }
-    it { expect(subject).to be_a(OAuth2::AccessToken) }
+  end
+
+  describe "#post" do
+    let(:endpoint) { "/ping" }
+
+    subject { client.post(endpoint, body:{message:"Hello World"}) }
+
+    context "when successful" do
+      before do
+        client.oauth_client.connection = stub_call(:post,
+                                                   credentials[:url] + endpoint ) {
+          [200, {'Content-Type'=>'application/json'},
+           read_fixture("waytostay#{endpoint}.post.json")]
+        }
+      end
+
+      it { expect(subject.value).to be_a(Result) }
+      it { expect(subject).to be_success }
+    end
   end
 end
 
