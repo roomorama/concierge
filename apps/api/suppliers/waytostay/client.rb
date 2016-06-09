d22e7f4f9bafe4eae82553f91fc59be318be0a52
keang <skeang@gmail.com>
2016-06-09 23:37:57 +0800

    Remove unused stuff

--- apps/api/suppliers/waytostay/client.rb
+++ apps/api/suppliers/waytostay/client.rb
@@ -30,7 +30,6 @@ module Waytostay
     # Waytostay, a generic error message is sent back to the caller, and the failure
     # is logged.
     def quote(params)
-      #return unavailable_quotation if params.stay_length > MAXIMUM_STAY_LENGTH
       result = oauth2_client.post(ENDPOINTS[:quote])
 
       if result.success?
@@ -56,24 +55,6 @@ module Waytostay
         }
     end
 
-    # Always returns a +Reservation+.
-    # If an error happens in any step in the process of getting a response back from
-    # JTB, a generic error message is sent back to the caller, and the failure
-    # is logged.
-    def book(params)
-      result = JTB::Booking.new(credentials).book(params)
-      if result.success?
-        reservation = Reservation.new(params)
-        reservation.code = result.value
-
-        # workaround to keep booking code for reservation. Returns reservation
-        database.create(reservation)
-      else
-        announce_error("booking", result)
-        Reservation.new(errors: { booking: 'Could not book property with remote supplier' })
-      end
-    end
-
     def oauth2_client
       @oauth2_client ||= API::Support::OAuth2Client.new(id: credentials[:client_id],
                                                         secret: credentials[:client_secret],
@@ -93,14 +74,6 @@ module Waytostay
         context:     API.context.to_h,
         happened_at: Time.now
       })
-    end
-
-    def database
-      @database ||= Concierge::OptionalDatabaseAccess.new(ReservationRepository)
-    end
-
-    def unavailable_quotation
-      Quotation.new(errors: { quote: "Maximum length of stay must be less than #{MAXIMUM_STAY_LENGTH} nights." })
     end
 
   end
