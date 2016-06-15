b4f365cab967361ca8bba9d258717be8486b9dbf
Igor Trofimov <igor@roomorama.com>
2016-06-15 08:28:31 +0800

    add comments

--- lib/concierge/suppliers/atleisure/importer.rb
+++ lib/concierge/suppliers/atleisure/importer.rb
@@ -1,4 +1,14 @@
 module AtLeisure
+  # +AtLeisure::Importer+
+  #
+  # This class wraps supplier API and provides data for building properties.
+  #
+  # Usage
+  #
+  #   importer = AtLeisure::Importer.new(credentials)
+  #   importer.fetch_properties
+  #
+  #   => #<Result:0x007ff5fc624dd8 @result=[{'HouseCode' => 'XX-12345-67', ...}, ...]
   class Importer
 
     ENDPOINT_METHODS = {
@@ -7,18 +17,7 @@ module AtLeisure
       layout_items:    "ReferenceLayoutItemsV1"
     }
 
-    LAYERS = [
-      "BasicInformationV3",
-      "MediaV1",
-      "LanguagePackDEV3",
-      "LanguagePackENV3",
-      "LanguagePackESV3",
-      "PropertiesV1",
-      "LayoutExtendedV2",
-      "DistancesV1",
-      "AvailabilityPeriodV1",
-      "MinMaxPriceV1"
-    ]
+    LAYERS = %w(BasicInformationV3 MediaV1 LanguagePackDEV3 LanguagePackENV3 LanguagePackESV3 PropertiesV1 LayoutExtendedV2 DistancesV1 AvailabilityPeriodV1 MinMaxPriceV1)
 
     attr_reader :credentials
 
@@ -26,17 +25,20 @@ module AtLeisure
       @credentials = credentials
     end
 
+    # this method retrieves the list of properties
     def fetch_properties
       endpoint = ENDPOINT_METHODS.fetch(:properties_list)
       client_for(endpoint).invoke(endpoint, authentication_params)
     end
 
-
+    # fetches references information for properties. Return result with array
     def fetch_layout_items
       endpoint = ENDPOINT_METHODS.fetch(:layout_items)
       client_for(endpoint).invoke(endpoint, authentication_params)
     end
 
+    # fetches extended information for properties by their identifiers. Return result with list of properties with
+    # additional data which were pointed in +layers+
     def fetch_data(properties, layers: LAYERS)
       params   = { 'HouseCodes' => identifiers(properties), 'Items' => Array(layers) }
       endpoint = ENDPOINT_METHODS.fetch(:properties_data)
