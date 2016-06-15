e9eb2f0efec78c5e4bef747ad6ed2ea71d091e39
Keang <skeang@gmail.com>
2016-06-15 11:51:43 +0800

    Refactor waytostay client into modules

--- lib/concierge/suppliers/waytostay/client.rb
+++ lib/concierge/suppliers/waytostay/client.rb
@@ -1,3 +1,4 @@
+require_relative 'quote'
 module Waytostay
   # +Waytostay::Client+
   #
@@ -13,48 +14,16 @@ module Waytostay
   #
   # For more information on how to interact with Waytostay, check the project Wiki.
   class Client
+
     SUPPLIER_NAME = "Waytostay"
-    ENDPOINTS = {
-      quote: "/bookings/quote",
-    }
-    UNAVAILBLE_ERROR_MESSAGE = "Apartment is not available for the selected dates"
+
+    include Waytostay::Quote
 
     attr_reader :credentials
 
     # credentails should include client_id and client_secret
     def initialize
       @credentials = Concierge::Credentials.for("waytostay")
-    end
-
-    # Always returns a +Quotation+.
-    # If an error happens in any step in the process of getting a response back from
-    # Waytostay, a generic error message is sent back to the caller, and the failure
-    # is logged.
-    def quote(params)
-      post_body = {
-        property_reference: params.fetch(:property_id),
-        arrival_date: params.fetch(:check_in),
-        departure_date: params.fetch(:check_out),
-        number_of_adults: params.fetch(:guests)
-      }
-      result = oauth2_client.post(ENDPOINTS[:quote],
-                                 body: post_body.to_json,
-                                 headers: headers)
-
-      if result.success?
-        Quotation.new(quote_params_from(result.value))
-      elsif unavailable?(result)
-        Quotation.new({
-          property_id: params[:property_id],
-          check_in: params[:check_in],
-          check_out: params[:check_out],
-          guests: params[:guests],
-          available: false
-        })
-      else
-        announce_error("quote", result)
-        Quotation.new(errors: { quote: "Could not quote price with remote supplier" })
-      end
     end
 
     def oauth2_client
@@ -65,24 +34,6 @@ module Waytostay
     end
 
     private
-
-    def unavailable?(result)
-      result.error.data && result.error.data.include?(UNAVAILBLE_ERROR_MESSAGE)
-    end
-
-    def quote_params_from(json)
-      details = json["booking_details"]
-      {
-        property_id: details["property_reference"],
-        check_in: details["arrival_date"],
-        check_out: details["departure_date"],
-        guests: details["number_of_adults"], #should we add infants?
-        fee: details["price"]["pricing_summary"]["agency"]["commission_amount"],
-        total: details["price"]["pricing_summary"]["final_price"],
-        currency: details["price"]["currency"],
-        available: true,
-      }
-    end
 
     def headers
       {
