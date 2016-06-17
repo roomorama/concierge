c350ca7cd3b64e76891e8e38eacbc851854ef171
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-17 12:48:25 +0600

    SAW: booking

---
+++ apps/api/controllers/saw/booking.rb
@@ -0,0 +1,24 @@
+require_relative "../booking"
+
+module API::Controllers::SAW
+
+  # +API::Controllers::AtLeisure::Booking+
+  #
+  # Performs create booking for properties from SAW.
+  class Booking
+    include API::Controllers::Booking
+
+    # Make property booking request 
+    #
+    # Usage
+    #
+    #   It returns a +Reservation+ object in both success and fail cases:
+    #   
+    #   API::Controllers::SAW::Booking.create_booking(selected_params)
+    #   => Reservation(..)
+    def create_booking(params)
+      credentials = Concierge::Credentials.for("SAW")
+      SAW::Client.new(credentials).book(params)
+    end
+  end
+end
---
+++ lib/concierge/suppliers/saw/mappers/reservation.rb
@@ -0,0 +1,17 @@
+module SAW
+  module Mappers
+    class Reservation
+      def self.build(request_params, result_hash)
+        reservation_code = parse_reservation_code(result_hash)
+
+        ::Reservation.new(request_params.merge!(code: reservation_code))
+      end
+
+      private
+      def self.parse_reservation_code(hash)
+        hash.fetch("response")
+            .fetch("booking_ref_number")
+      end
+    end
+  end
+end
