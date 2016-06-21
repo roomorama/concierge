5ec27439674033c5ca75a0314862117c3230edbe
Igor Trofimov <igor@roomorama.com>
2016-06-21 13:54:58 +0800

    subscribe suppliers from yml

--- apps/workers/config/initializers/subscribe_suppliers.rb
+++ apps/workers/config/initializers/subscribe_suppliers.rb
@@ -1,5 +1,6 @@
-suppliers = SupplierRepository.all
-suppliers.each do |supplier|
+path  = Hanami.root.join("config", "suppliers.yml").to_s
+names = YAML.load_file(path) || [] # if the file is empty, +load_file+ returns +false+
+names.each do |supplier|
   Concierge::Announcer.on("sync.#{supplier.name}") do |host|
     Workers::Suppliers::AtLeisure.new(host).perform
   end
