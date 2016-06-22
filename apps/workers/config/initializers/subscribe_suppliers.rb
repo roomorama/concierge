448e40557c8d41b1e5bd77539216538469310e0c
Igor Trofimov <igor@roomorama.com>
2016-06-22 09:05:51 +0800

    fix worker

--- apps/workers/config/initializers/subscribe_suppliers.rb
+++ apps/workers/config/initializers/subscribe_suppliers.rb
@@ -1,7 +1,3 @@
-path  = Hanami.root.join("config", "suppliers.yml").to_s
-suppliers = YAML.load_file(path) || [] # if the file is empty, +load_file+ returns +false+
-suppliers.each do |supplier|
-  Concierge::Announcer.on("sync.#{supplier}") do |host|
-    Workers::Suppliers::AtLeisure.new(host).perform
-  end
+Concierge::Announcer.on("sync.AtLeisure") do |host|
+  Workers::Suppliers::AtLeisure.new(host).perform
 end
