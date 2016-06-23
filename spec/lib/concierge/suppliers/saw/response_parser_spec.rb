d3a3fa7e0691462c420e07d04343c30e75e1900c
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-23 15:27:45 +0600

    SAW: specs for ResponseParser

---
+++ spec/lib/concierge/suppliers/saw/response_parser_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+RSpec.describe SAW::ResponseParser do
+  let(:xml_response) do
+    "<response>Some Response</response>"
+  end
+
+  let(:bad_xml_response) { 'Server Error' }
+
+  it "converts XML response to a safe hash object with given attributes" do
+    response_parser = described_class.new
+    hash = response_parser.to_hash(xml_response)
+
+    expect(hash).to be_kind_of(Concierge::SafeAccessHash)
+    expect(hash.get("response")).to eq("Some Response")
+    expect(hash.to_h).to eq({ "response" => "Some Response" })
+  end
+
+  it "returns an empty object in case when XML format is not correct" do
+    response_parser = described_class.new
+    hash = response_parser.to_hash(bad_xml_response)
+
+    expect(hash).to be_kind_of(Concierge::SafeAccessHash)
+    expect(hash.to_h).to eq({})
+  end
+end
