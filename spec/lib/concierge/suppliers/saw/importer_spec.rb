15c5c9351aec0094637afc2f28dbffa7d337163b
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-24 16:34:29 +0600

    SAW: documentation & specs for SAW::Importer

--- spec/lib/concierge/suppliers/saw/importer_spec.rb
+++ spec/lib/concierge/suppliers/saw/importer_spec.rb
@@ -1,5 +1,7 @@
 require "spec_helper"
 
+# Just assertions that returned objects is correct-typed.
+# Country attributes mapping specs are in corresponding Mapper class spec.
 RSpec.describe SAW::Importer do
   include Support::HTTPStubbing
   include Support::Fixtures
@@ -7,6 +9,32 @@ RSpec.describe SAW::Importer do
 
   let(:credentials) { Concierge::Credentials.for("SAW") }
   let(:subject) { described_class.new(credentials) }
+
+  describe "fetch_countries" do
+    it "returns country objects" do
+      mock_request(:country, :multiple)
+      countries_result = subject.fetch_countries
+
+      expect(countries_result.value).to all(be_kind_of(SAW::Entities::Country))
+    end
+  end
+  
+  describe "fetch_properties_by_country" do
+    it "returns a result with an error if there is no results" do
+      mock_request(:country, :one)
+      mock_request(:propertysearch, :empty)
+    
+      countries_result = subject.fetch_countries
+      current_country = countries_result.value.first
+
+      properties_result = subject.fetch_properties_by_country(current_country)
+      expect(properties_result.success?).to be false
+      expect(properties_result.error.code).to eq("1007")
+      expect(properties_result.error.data).to eq(
+        "No properties are available for the given search parameters"
+      )
+    end
+  end
 
   describe "fetch_properties_by_countries" do
     it "returns an empty array when all requests are failed / empty" do
