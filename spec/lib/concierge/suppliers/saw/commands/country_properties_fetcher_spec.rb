fac61e298944a00b7c56c3adcfc0be34257660c5
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-24 12:21:15 +0600

    SAW: extract mock_request to support module

--- spec/lib/concierge/suppliers/saw/commands/country_properties_fetcher_spec.rb
+++ spec/lib/concierge/suppliers/saw/commands/country_properties_fetcher_spec.rb
@@ -3,6 +3,7 @@ require "spec_helper"
 RSpec.describe SAW::Commands::CountryPropertiesFetcher do
   include Support::HTTPStubbing
   include Support::Fixtures
+  include Support::SAW::MockRequest
 
   let(:credentials) { Concierge::Credentials.for("SAW") }
   let(:subject) { described_class.new(credentials) }
@@ -141,15 +142,5 @@ RSpec.describe SAW::Commands::CountryPropertiesFetcher do
 
       expect(multi_unit_values).to all(eq(true))
     end
-  end
-
-  private
-  def mock_request(endpoint, filename)
-    stub_data = read_fixture("saw/#{endpoint}/#{filename}.xml")
-    stub_call(:post, endpoint_for(endpoint)) { [200, {}, stub_data] }
-  end
-
-  def endpoint_for(method)
-    "http://staging.servicedapartmentsworldwide.net/xml/#{method}.aspx"
   end
 end
