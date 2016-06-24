e690a77d4e94de47bd84779a5aa4a70e35ee664e
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-24 15:54:25 +0600

    SAW: move this spec under proper describe block

--- spec/lib/concierge/suppliers/saw/importer_spec.rb
+++ spec/lib/concierge/suppliers/saw/importer_spec.rb
@@ -8,16 +8,16 @@ RSpec.describe SAW::Importer do
   let(:credentials) { Concierge::Credentials.for("SAW") }
   let(:subject) { described_class.new(credentials) }
 
-  it "returns an empty array when all requests are failed / empty" do
-    mock_request(:country, :multiple)
-    mock_request(:propertysearch, :empty)
-  
-    countries_result = subject.fetch_countries
-    properties = subject.fetch_properties_by_countries(countries_result.value)
-    expect(properties.size).to eq(0)
-  end
-
   describe "fetch_properties_by_countries" do
+    it "returns an empty array when all requests are failed / empty" do
+      mock_request(:country, :multiple)
+      mock_request(:propertysearch, :empty)
+    
+      countries_result = subject.fetch_countries
+      properties = subject.fetch_properties_by_countries(countries_result.value)
+      expect(properties.size).to eq(0)
+    end
+
     it "returns a result with array of all properties from all countries" do
       mock_request(:propertysearch, :success)
       mock_request(:country, :multiple)
