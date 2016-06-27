cdec5b734eed858c2b74afcd7d70cfb8ebf0fdb3
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-27 11:52:13 +0600

    SAW: add routes for controllers for quote and booking requests

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -4,12 +4,14 @@ post '/kigo/quote',          to: 'kigo#quote'
 post '/kigo/legacy/quote',   to: 'kigo/legacy#quote'
 post '/poplidays/quote',     to: 'poplidays#quote'
 post '/waytostay/quote',     to: 'waytostay#quote'
+post '/saw/quote',           to: 'saw#quote'
 
 post '/jtb/booking',         to: 'j_t_b#booking'
 post '/atleisure/booking',   to: 'at_leisure#booking'
 post '/waytostay/booking',   to: 'waytostay#booking'
 post '/kigo/booking',        to: 'kigo#booking'
 post '/kigo/legacy/booking', to: 'kigo/legacy#booking'
+post '/saw/booking',         to: 'saw#booking'
 
 post 'waytostay/cancel', to: 'waytostay#cancel'
 
