ac6950e275f2053008d4702547956121ed2866e5
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-06-29 16:20:24 +0600

    SAW: URLRewriter changes

--- lib/concierge/suppliers/saw/mappers/roomorama_image_set.rb
+++ lib/concierge/suppliers/saw/mappers/roomorama_image_set.rb
@@ -13,30 +13,38 @@ module SAW
       #
       #   * +hash+ [Concierge::SafeAccessHash] property hash with images
       #                                        information
-      #   * +image_url_rewrite+ [Boolan] whether rewrite image URLs or not
+      #   * +url_rewrite+ [Boolan] whether rewrite image URLs or not
       #
       # Returns [Array<Roomorama::Image] array of images
-      def self.build(hash, image_url_rewrite)
+      def self.build(hash, url_rewrite)
         images = hash.get("image_gallery.image")
         
         return [] unless images
         
         to_array(images).map do |h| 
           safe_hash = Concierge::SafeAccessHash.new(h)
-          build_image(safe_hash, image_url_rewrite)
+          build_image(safe_hash, url_rewrite)
         end
       end
 
       private
-      def self.build_image(hash, image_url_rewrite)
+      def self.build_image(hash, url_rewrite)
         url = hash.get("large_image_url").to_s
         title = hash.get("title").to_s.strip
 
         identifier = hash.get("id") || Digest::MD5.hexdigest(url)
         image = Roomorama::Image.new(identifier)
-        image.url = Converters::URLRewriter.build(url, rewrite: image_url_rewrite)
+        image.url = prepare_url(url, url_rewrite)
         image.caption = title
         image
+      end
+      
+      def self.prepare_url(url, url_rewrite)
+        if url_rewrite
+          Converters::URLRewriter.build(url)
+        else
+          url
+        end
       end
         
       def self.to_array(something)
