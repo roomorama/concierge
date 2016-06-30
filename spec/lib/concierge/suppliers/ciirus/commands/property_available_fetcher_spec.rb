14ce64681646745a16f5d8c9e75df11884365d8d
K.Kolotyuk <kk@7bits.com>
2016-06-30 15:47:30 +0200

    Prevent remote wsdl call with static file in ciirus specs

--- spec/lib/concierge/suppliers/ciirus/commands/property_available_fetcher_spec.rb
+++ spec/lib/concierge/suppliers/ciirus/commands/property_available_fetcher_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Ciirus::Commands::PropertyAvailableFetcher do
   include Support::Fixtures
   include Savon::SpecHelper
 
+  before { savon.mock! }
+  after { savon.unmock! }
+
   let(:credentials) do
     double(username: 'Foo',
            password: '123',
@@ -12,17 +15,27 @@ RSpec.describe Ciirus::Commands::PropertyAvailableFetcher do
   end
 
   let(:params) do
-    API::Controllers::Params::Quote.new(property_id: 38180,
-                                        check_in: '2016-05-01',
-                                        check_out: '2016-05-12',
-                                        guests: 3)
+    {
+      property_id: 38180,
+      check_in: '2016-05-01',
+      check_out: '2016-05-12',
+      guests: 3
+    }
   end
 
-  before { savon.mock! }
-  after { savon.unmock! }
-
+  let(:wsdl) { read_fixture('ciirus/wsdl.xml') }
   let(:success_response) { read_fixture('ciirus/is_property_available_response.xml') }
+
   subject { described_class.new(credentials) }
+
+  before do
+    # Replace remote call for wsdl with static wsdl
+    allow(subject).to receive(:options).and_wrap_original do |m, *args|
+      original = m.call
+      original[:wsdl] = wsdl
+      original
+    end
+  end
 
   describe '#call' do
     it 'returns property availability' do
