352a120e2b57a7e2aae056a360632223b6b843d5
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-04 21:13:15 +0200

    Add make booking call for ciirus supplier

---
+++ spec/lib/concierge/suppliers/ciirus/commands/booking_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+RSpec.describe Ciirus::Commands::Booking do
+  include Support::Fixtures
+  include Support::SOAPStubbing
+
+  let(:credentials) do
+    double(username: 'Foo',
+           password: '123',
+           url:      'http://example.org')
+  end
+
+  let(:customer) do
+    {
+      first_name: 'John',
+      last_name:  'Butler',
+      email:      'john@email.com',
+      phone:      '+3 5486 4560',
+      address:    'Long Island 1245'
+    }
+  end
+
+  let(:params) do
+    API::Controllers::Params::Booking.new(
+      property_id: 38180,
+      check_in:    '2016-05-01',
+      check_out:   '2016-05-12',
+      guests:      3,
+      subtotal:    3000.0,
+      customer:    customer
+    )
+  end
+
+  let(:success_response) { read_fixture('ciirus/make_booking_response.xml') }
+  let(:error_response) { read_fixture('ciirus/error_make_booking_response.xml') }
+  let(:wsdl) { read_fixture('ciirus/wsdl.xml') }
+
+  subject { described_class.new(credentials) }
+
+  before do
+    # Replace remote call for wsdl with static wsdl
+    allow(subject).to receive(:options).and_wrap_original do |m, *args|
+      original = m.call
+      original[:wsdl] = wsdl
+      original
+    end
+  end
+
+  describe '#call' do
+    context 'when xml response is correct' do
+      it 'returns success reservation' do
+        stub_call(method: :make_booking, response: success_response)
+
+        result = subject.call(params)
+
+        expect(result).to be_a Result
+        expect(result).to be_success
+        expect(result.value).to be_a Reservation
+      end
+
+      it 'fills reservation with right attributes' do
+        stub_call(method: :make_booking, response: success_response)
+
+        result = subject.call(params)
+
+        reservation = result.value
+        expect(reservation.check_in).to eq('2016-05-01')
+        expect(reservation.check_out).to eq('2016-05-12')
+        expect(reservation.guests).to eq(3)
+        expect(reservation.property_id).to eq('38180')
+        expect(reservation.code).to eq('873184')
+        expect(reservation.customer).to eq(customer)
+      end
+    end
+
+    context 'when xml contains error message' do
+      it 'returns a result with error' do
+        stub_call(method: :make_booking, response: error_response)
+
+        result = subject.call(params)
+
+        expect(result.success?).to be false
+        expect(result.error.code).to eq(:unexpected_response)
+      end
+    end
+  end
+end
---
+++ spec/lib/concierge/suppliers/ciirus/mappers/reservation_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+
+RSpec.describe Ciirus::Mappers::Reservation do
+
+  let(:customer) do
+    {
+      first_name: 'John',
+      last_name:  'Butler',
+      email:      'john@email.com',
+      phone:      '+3 5486 4560',
+      address:    'Long Island 1245'
+    }
+  end
+
+  let(:params) do
+    API::Controllers::Params::Booking.new(
+      property_id: 38180,
+      check_in:    '2016-05-01',
+      check_out:   '2016-05-12',
+      guests:      3,
+      subtotal:    3000.0,
+      customer:    customer
+    )
+  end
+
+  context 'for valid result hash' do
+    let(:result_hash) do
+      Concierge::SafeAccessHash.new(
+        {
+          make_booking_response: {
+            make_booking_result: {
+              booking_placed: true,
+              error_message: nil,
+              booking_id: "873184",
+              total_amount_including_tax: "2858.850000"
+            }
+          }
+        }
+      )
+    end
+
+    let(:reservation) { described_class.build(params, result_hash) }
+
+    it 'returns mapped roomorama quotation entity' do
+      expect(reservation).to be_a(::Reservation)
+      expect(reservation.check_in).to eq('2016-05-01')
+      expect(reservation.check_out).to eq('2016-05-12')
+      expect(reservation.guests).to eq(3)
+      expect(reservation.property_id).to eq('38180')
+      expect(reservation.code).to eq('873184')
+      expect(reservation.customer).to eq(customer)
+    end
+  end
+end
