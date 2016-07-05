c0341b1c3e51a75451a923d766169b23055c1c6e
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-05 12:02:40 +0600

    SAW: add shared spec examples for SAW controllers

--- spec/api/controllers/saw/booking_spec.rb
+++ spec/api/controllers/saw/booking_spec.rb
@@ -1,55 +1,56 @@
 require "spec_helper"
-require_relative "../shared/quote_validations"
-require_relative "../shared/external_error_reporting"
+require_relative "../shared/booking_validations"
 
 RSpec.describe API::Controllers::SAW::Booking do
   include Support::HTTPStubbing
   include Support::Fixtures
   include Support::SAW::MockRequest
   
-  let(:request_attributes) do
+  let(:params) do
     {
       property_id: '1',
       unit_id: '9733',
       check_in: '02/02/2016',
       check_out: '03/02/2016',
-      num_guests: 1,
+      guests: 1,
       currency_code: 'EUR',
-      total: '123.45',
-      user: {
-        firstname: 'Test',
-        lastname: 'User',
+      subtotal: '123.45',
+      customer: {
+        first_name: 'Test',
+        last_name: 'User',
         email: 'testuser@example.com',
         display: 'Test User'
       }
     }
   end
 
-  let(:request_params) { Concierge::SafeAccessHash.new(request_attributes) }
+  let(:safe_params) { Concierge::SafeAccessHash.new(params) }
   let(:controller) { described_class.new }
   let(:action) { :booking }
+  
+  it_behaves_like "performing booking parameters validations", controller_generator: -> { described_class.new }
 
   it "returns result object with reservation if booking request is completed successfully" do
     mock_request(:propertybooking, :success)
     
-    result = controller.create_booking(request_params)
+    result = controller.create_booking(safe_params)
     
     expect(result.success?).to be true
     expect(result).to be_kind_of(Result)
 
     reservation = result.value
     expect(reservation).to be_kind_of(Reservation)
-    expect(reservation.property_id).to eq(request_params[:property_id])
-    expect(reservation.unit_id).to eq(request_params[:unit_id])
-    expect(reservation.check_in).to eq(request_params[:check_in])
-    expect(reservation.check_out).to eq(request_params[:check_out])
+    expect(reservation.property_id).to eq(safe_params[:property_id])
+    expect(reservation.unit_id).to eq(safe_params[:unit_id])
+    expect(reservation.check_in).to eq(safe_params[:check_in])
+    expect(reservation.check_out).to eq(safe_params[:check_out])
     expect(reservation.code).to eq('MTA66395')
   end
 
   it "returns an error reservation if booking request fails" do
     mock_request(:propertybooking, :error)
     
-    result = controller.create_booking(request_params)
+    result = controller.create_booking(safe_params)
     
     expect(result.success?).to be false
     expect(result).to be_kind_of(Result)
@@ -60,7 +61,7 @@ RSpec.describe API::Controllers::SAW::Booking do
     it "returns a reservation with an appropriate error" do
       mock_bad_xml_request(:propertybooking)
 
-      result = controller.create_booking(request_params)
+      result = controller.create_booking(safe_params)
       
       expect(result.success?).to be false
       expect(result).to be_kind_of(Result)
