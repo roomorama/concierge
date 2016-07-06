da313514d5db977845eca88da7024dd39ace0612
Keang <skeang@gmail.com>
2016-07-06 15:55:08 +0800

    Update specs

--- spec/lib/concierge/roomorama/client/operations/calendar_update_spec.rb
+++ spec/lib/concierge/roomorama/client/operations/calendar_update_spec.rb
@@ -6,20 +6,16 @@ RSpec.describe Roomorama::Client::Operations::CalendarUpdate do
   subject { described_class.new(property) }
 
   describe "#endpoint" do
-    it "knows the endpoint where a property can be disabled" do
+    it "knows the endpoint to update availabilities" do
       expect(subject.endpoint).to eq "/v1.0/host/rooms/1/availabilities"
     end
   end
 
   describe "#method" do
-    it "knows the request method to be used when publishing" do
+    it "knows the request method to update availabilities" do
       expect(subject.request_method).to eq :put
     end
   end
 
-  describe "#request_data" do
-    it "serializes the property identifier" do
-      expect(subject.request_data).to eq({})
-    end
-  end
+  describe "#request_data"
 end
