0c4ba6cd83fee7b438d2ecba3d76f2244fde559d
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-07 17:14:02 +0200

    Add image list fetcher for ciirus api

---
+++ lib/concierge/suppliers/ciirus/commands/image_list_fetcher.rb
@@ -0,0 +1,47 @@
+module Ciirus
+  module Commands
+    #  +Ciirus::ImageListFetcher+
+    #
+    # This class is responsible for fetching a property image list
+    # from Ciirus API, parsing the response and building the result.
+    #
+    # Usage
+    #
+    #   result = Ciirus::Commands::ImageListFetcher.new(credentials).fetch(params)
+    #   if result.success?
+    #     result.value
+    #   end
+    #
+    # The +call+ method returns a +Result+ object that, when successful,
+    # encapsulates the collection of image urls.
+    class ImageListFetcher < BaseCommand
+
+      def call(property_id)
+        message = xml_builder.image_list(property_id)
+        result = remote_call(message)
+        if result.success?
+          result_hash = to_safe_hash(result.value)
+          image_list = build_image_list(result_hash)
+          Result.new(image_list)
+        else
+          result
+        end
+      end
+
+      protected
+
+      def operation_name
+        :get_image_list
+      end
+
+      private
+
+      def build_image_list(rates_hash)
+        images = rates_hash.get(
+          'get_image_list_response.get_image_list_result.string'
+        )
+        images ? Array(images) : []
+      end
+    end
+  end
+end
---
+++ spec/lib/concierge/suppliers/ciirus/commands/image_list_fetcher_spec.rb
@@ -0,0 +1,93 @@
+require 'spec_helper'
+
+RSpec.describe Ciirus::Commands::ImageListFetcher do
+  include Support::Fixtures
+  include Support::SOAPStubbing
+
+  let(:credentials) do
+    double(username: 'Foo',
+           password: '123',
+           url:      'example.org')
+  end
+
+  let(:property_id){ 38180 }
+
+  let(:success_response) { read_fixture('ciirus/image_list_response.xml') }
+  let(:one_image_response) { read_fixture('ciirus/one_image_list_response.xml') }
+  let(:empty_response) { read_fixture('ciirus/empty_image_list_response.xml') }
+  let(:wsdl) { read_fixture('ciirus/wsdl.xml') }
+  subject { described_class.new(credentials) }
+
+  before do
+    # Replace remote call for wsdl with static wsdl
+    allow(subject).to receive(:options).and_wrap_original do |m, *args|
+      original = m.call
+      original[:wsdl] = wsdl
+      original
+    end
+  end
+
+  describe '#call' do
+
+    context 'when remote call internal error happened' do
+      it 'returns result with error' do
+        allow_any_instance_of(Savon::Client).to receive(:call) { raise Savon::Error }
+        result = subject.call(property_id)
+
+        expect(result).not_to be_success
+        expect(result.error.code).to eq :savon_error
+      end
+    end
+
+    context 'when many images' do
+      let(:many_images) do
+        [
+          'http://images.ciirus.com/properties/25559/51502/images/ccpdemo1.jpg',
+          'http://images.ciirus.com/properties/25559/51502/images/ccpdemo2.jpg',
+          'http://images.ciirus.com/properties/25559/51502/images/ccpdemo3.jpg'
+        ]
+      end
+
+      it 'returns array of images' do
+        stub_call(method: :get_image_list, response: success_response)
+
+        result = subject.call(property_id)
+        images = result.value
+
+        expect(result).to be_a Result
+        expect(result).to be_success
+        expect(images).to contain_exactly(*many_images)
+      end
+    end
+
+    context 'when one image' do
+      let(:one_image) do
+        [
+          'http://images.ciirus.com/properties/25559/51502/images/ccpdemo1.jpg'
+        ]
+      end
+
+      it 'returns array with a image' do
+        stub_call(method: :get_image_list, response: one_image_response)
+
+        result = subject.call(property_id)
+        images = result.value
+
+        expect(result).to be_a Result
+        expect(result).to be_success
+        expect(images).to contain_exactly(*one_image)
+      end
+    end
+
+    it 'returns empty array for empty response' do
+      stub_call(method: :get_image_list, response: empty_response)
+
+      result = subject.call(property_id)
+      images = result.value
+
+      expect(result).to be_a Result
+      expect(result).to be_success
+      expect(images).to be_empty
+    end
+  end
+end
