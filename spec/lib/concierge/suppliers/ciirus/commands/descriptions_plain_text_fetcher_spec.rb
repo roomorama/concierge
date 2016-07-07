2229282ba05b9a977c6fa6e4b58902f736fff53a
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-07 18:19:39 +0200

    Add property description fetcher for ciirus api

---
+++ spec/lib/concierge/suppliers/ciirus/commands/descriptions_plain_text_fetcher_spec.rb
@@ -0,0 +1,50 @@
+require 'spec_helper'
+
+RSpec.describe Ciirus::Commands::DescriptionsPlainTextFetcher do
+  include Support::Fixtures
+  include Support::SOAPStubbing
+
+  let(:credentials) do
+    double(username: 'Foo',
+           password: '123',
+           url:      'http://proxy.roomorama.com/ciirus')
+  end
+
+  let(:property_id) { 38180 }
+
+  let(:wsdl) { read_fixture('ciirus/wsdl.xml') }
+  let(:success_response) { read_fixture('ciirus/descriptions_plain_text_response.xml') }
+
+  subject { described_class.new(credentials) }
+
+  before do
+    # Replace remote call for wsdl with static wsdl
+    allow(subject).to receive(:options).and_wrap_original do |m, *args|
+      original = m.call
+      original[:wsdl] = wsdl
+      original
+    end
+  end
+
+  describe '#call' do
+    context 'when remote call internal error happened' do
+      it 'returns result with error' do
+        allow_any_instance_of(Savon::Client).to receive(:call) { raise Savon::Error }
+        result = subject.call(property_id)
+
+        expect(result).not_to be_success
+        expect(result.error.code).to eq :savon_error
+      end
+    end
+
+    it 'returns descriptions' do
+      stub_call(method: :get_descriptions_plain_text, response: success_response)
+
+      result = subject.call(property_id)
+
+      expect(result).to be_a Result
+      expect(result).to be_success
+      expect(result.value).to eq 'Some description here'
+    end
+  end
+end
