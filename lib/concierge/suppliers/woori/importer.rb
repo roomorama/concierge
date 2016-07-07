3141a3b46282d94440c96c0b8cf9ae93b33399da
benhawker <ben.c.hawker@gmail.com>
2016-07-07 11:03:21 +0800

    Starts woori - adds importer and suppliers.yml

---
+++ lib/concierge/suppliers/woori/importer.rb
@@ -0,0 +1,15 @@
+module Woori
+  # +Woori::Importer+
+  #
+  # This class wraps supplier API and provides data for building properties.
+  #
+  # Usage
+  #
+  #   importer = Woori::Importer.new(credentials)
+  #   importer.fetch_properties
+  #
+  #   => RESULT - TODO
+  class Importer
+
+  end
+end
