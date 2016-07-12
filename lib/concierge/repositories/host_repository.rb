739334259cbd0586345b87b40cfec034c3090f89
Renato Mascarenhas <renato@roomorama.com>
2016-07-12 11:12:45 +0800

    Ability to query for hosts by identifier and supplier.

--- lib/concierge/repositories/host_repository.rb
+++ lib/concierge/repositories/host_repository.rb
@@ -8,4 +8,19 @@ class HostRepository
   def self.count
     query.count
   end
+
+  # queries for all hosts that belong to a given +supplier+
+  def self.from_supplier(supplier)
+    query do
+      where(supplier_id: supplier.id)
+    end
+  end
+
+  # queries for a host with the given +identifier+, which should be unique
+  # per supplier.
+  def self.identified_by(identifier)
+    query do
+      where(identifier: identifier)
+    end
+  end
 end
--- spec/lib/concierge/repositories/host_repository_spec.rb
+++ spec/lib/concierge/repositories/host_repository_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe HostRepository do
       expect(described_class.count).to eq 1
     end
   end
+
+  describe ".from_supplier" do
+    let(:supplier) { create_supplier }
+
+    it "returns an empty collection if there are no hosts for a given supplier" do
+      expect(described_class.from_supplier(supplier).to_a).to eq []
+    end
+
+    it "returns only hosts that belong to the given supplier" do
+      host_from_supplier       = create_host(supplier_id: supplier.id)
+      host_from_other_supplier = create_host
+
+      expect(described_class.from_supplier(supplier).to_a).to eq [host_from_supplier]
+    end
+  end
+
+  describe ".identified_by" do
+    it "returns an empty collection when no hosts match the given identifier" do
+      expect(described_class.identified_by("identifier").to_a).to eq []
+    end
+
+    it "returns only hosts that match the given identifier" do
+      host = create_host(identifier: "host1")
+      expect(described_class.identified_by("host1").to_a).to eq [host]
+    end
+  end
 end
