a73c3ab3dc5c34f16a72eb653c3834f85c7c88fe
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-13 10:33:12 +0600

    Woori: add enpoint.rb + spec

---
+++ lib/concierge/suppliers/woori/endpoint.rb
@@ -0,0 +1,17 @@
+module Woori
+  # +Woori::Endpoint+
+  #
+  # This class provides mapping between internal endpoint names and 
+  # corresponding Woori API urls.
+  class Endpoint
+    ENDPOINTS = {
+      properties: "properties",
+    }
+
+    # Returns a URL part for a given endpoint by its system name
+    # Raises exception in case if endpoint could not be found
+    def self.endpoint_for(api_method)
+      ENDPOINTS.fetch(api_method)
+    end
+  end
+end
