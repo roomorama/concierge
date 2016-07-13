557bbdbe1cf39d6ba574ace545d5a4ef992f82d1
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-13 10:33:12 +0600

    Woori: add enpoint.rb + spec

---
+++ spec/lib/concierge/suppliers/woori/endpoint_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+RSpec.describe Woori::Endpoint do
+  it "returns an endpoint URL part by its system name" do
+    described_class::ENDPOINTS.each do |system_name, url|
+      expect(described_class.endpoint_for(system_name)).to eq(url)
+    end
+  end
+
+  it "raises an exception in case if endpoint could not be found" do
+    expect {
+      described_class.endpoint_for("unknown-endpoint")
+    }.to raise_error(KeyError)
+  end
+end
