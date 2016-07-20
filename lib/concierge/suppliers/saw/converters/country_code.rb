18625ef908750cdf0889729d7a2c57b8803a82e5
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-20 10:36:16 +0600

    SAW: immidiately return country code instead of prettyfying country name and then calling iso_country_code search

--- lib/concierge/suppliers/saw/converters/country_code.rb
+++ lib/concierge/suppliers/saw/converters/country_code.rb
@@ -7,12 +7,12 @@ module SAW
     # part of the library API.
     #
     # There is also one added step in country code convertion: this class adds
-    # a step for *prettyfying* country names because there is some differences 
-    # between which names are used in country codes converter library and in 
-    # SAW API.
+    # a step for resolving custom country codes because there is some 
+    # differences between which names are used in country codes converter 
+    # library and in SAW API.
     class CountryCode
-      CUSTOM_COUNTRY_NAMES_MAPPING = {
-        "Korea, Republic of" => "Korea (Republic of)",
+      CUSTOM_COUNTRY_CODES = {
+        "Korea, Republic of" => "KR",
       }
 
       class << self
@@ -28,14 +28,12 @@ module SAW
         # 
         # Returns [String] country code
         def code_by_name(name)
-          standartized_name = prepare_name(name)
-          country = IsoCountryCodes.search_by_name(standartized_name).first
-          (country && country.alpha2).to_s
-        end
+          custom_code = CUSTOM_COUNTRY_CODES[name]
 
-        private
-        def prepare_name(name)
-          CUSTOM_COUNTRY_NAMES_MAPPING.fetch(name, name)
+          return custom_code if custom_code
+
+          country = IsoCountryCodes.search_by_name(name).first
+          (country && country.alpha2).to_s
         end
       end
     end
