1a381912f355828c4da66dcc6d4a71380d93a5df
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-21 15:07:33 +0600

    Woori: sync worker for properties and its units

--- lib/concierge/suppliers/woori/importer.rb
+++ lib/concierge/suppliers/woori/importer.rb
@@ -20,8 +20,14 @@ module Woori
       properties_fetcher.call(updated_at, limit, offset)
     end
 
-    def fetch_units(property_hash)
-      []
+    def fetch_units(property_id)
+      properties_fetcher = Commands::UnitsFetcher.new(credentials)
+      properties_fetcher.call(property_id)
+    end
+    
+    def fetch_all_units(updated_at, limit, offset)
+      properties_fetcher = Commands::UnitsFetcher.new(credentials)
+      properties_fetcher.call(updated_at, limit, offset)
     end
   end
 end
