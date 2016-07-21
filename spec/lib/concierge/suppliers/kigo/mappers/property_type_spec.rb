568cd1f1d33c345d9455eaab26821787374f1f1a
igor <igortr333@gmail.com>
2016-07-21 17:44:07 +1000

    add property type mapper

---
+++ spec/lib/concierge/suppliers/kigo/mappers/property_type_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+RSpec.describe Kigo::Mappers::PropertyType do
+  include Support::Fixtures
+
+  let(:property_types) { JSON.parse(read_fixture('kigo/property_types.json')) }
+
+  subject { described_class.new(property_types) }
+
+  describe '#map' do
+    let(:property_type_id) { 4 }
+
+    it { expect(subject.map(123)).to eq nil }
+    it { expect(subject.map(nil)).to eq nil }
+    it { expect(subject.map(property_type_id)).to eq %w(house lake_house) }
+
+  end
+
+end
