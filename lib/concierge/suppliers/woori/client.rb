3e0698463655631880acd82fdd14ffbface1a500
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-22 12:11:07 +0600

    Woori: add quote controller

--- lib/concierge/suppliers/woori/client.rb
+++ lib/concierge/suppliers/woori/client.rb
@@ -13,5 +13,25 @@ module Woori
     def initialize(credentials)
       @credentials = credentials
     end
+    
+    # Quote Woori properties prices
+    # If an error happens in any step in the process of getting a response back
+    # from Woori, a result object with error is returned  
+    #
+    # Usage
+    #
+    #   comamnd = Woori::Client.new(credentials)
+    #   result = command.quote(params)
+    #
+    #   if result.sucess?
+    #     # ...
+    #   end
+    #
+    # Returns a +Result+ wrapping a +Quotation+ when operation succeeds
+    # Returns a +Result+ with +Result::Error+ when operation fails
+    def quote(params)
+      command = Woori::Commands::PriceFetcher.new(credentials)
+      command.call(params)
+    end
   end
 end
