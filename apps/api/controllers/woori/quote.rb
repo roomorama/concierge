67d57dab4a687ba4373e9bd0f7e5b7773c2d6eff
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-22 12:11:07 +0600

    Woori: add quote controller

---
+++ apps/api/controllers/woori/quote.rb
@@ -0,0 +1,30 @@
+require_relative "../quote"
+require_relative "../params/multi_unit_quote"
+
+module API::Controllers::Woori
+  # API::Controllers::Woori::Quote
+  #
+  # Performs booking quotations for properties from Woori.
+  class Quote
+    include API::Controllers::Quote
+    
+    params API::Controllers::Params::MultiUnitQuote
+
+    # Make price (property rate) request 
+    #
+    # Usage
+    #
+    #   It returns a Quotation object in both success and fail cases:
+    #   
+    #   API::Controllers::Woori::Quote.quote_price(selected_params)
+    #   => Quotation(..)
+    def quote_price(params)
+      credentials = Concierge::Credentials.for("Woori")
+      Woori::Client.new(credentials).quote(params)
+    end
+    
+    def supplier_name
+      Woori::Client::SUPPLIER_NAME
+    end
+  end
+end
