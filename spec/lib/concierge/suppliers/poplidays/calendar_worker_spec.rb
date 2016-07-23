3d0aaa0ccc402f0355c532dd497851f421c6c3d5
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-23 16:29:51 +0200

    Add poplidays calendar sync worker

---
+++ spec/lib/concierge/suppliers/poplidays/calendar_worker_spec.rb
@@ -0,0 +1,77 @@
+require 'spec_helper'
+
+RSpec.describe Workers::Suppliers::Poplidays::Calendar do
+  include Support::Fixtures
+  include Support::Factories
+
+  before(:example) { create_property(host_id: host.id) }
+
+  let(:supplier) { create_supplier(name: Poplidays::Client::SUPPLIER_NAME) }
+  let(:host) { create_host(supplier_id: supplier.id) }
+  let(:property_details) { parse_json(read_fixture('poplidays/property_details.json')) }
+  let(:availabilities) { parse_json(read_fixture('poplidays/availabilities_calendar.json')) }
+
+  subject { described_class.new(host) }
+
+  context 'fetching property details' do
+    before do
+      allow_any_instance_of(Poplidays::Importer).to receive(:fetch_property_details) { Result.error(:timeout_error) }
+    end
+
+    it 'announces an error if fetching property details fails' do
+      subject.perform
+
+      error = ExternalErrorRepository.last
+
+      expect(error.operation).to eq 'sync'
+      expect(error.supplier).to eq Poplidays::Client::SUPPLIER_NAME
+      expect(error.code).to eq 'timeout_error'
+    end
+
+    it 'doesnt finalize synchronisation with external error' do
+      expect(Roomorama::Client::Operations).to_not receive(:disable)
+      subject.perform
+    end
+  end
+
+  context 'fetching availabilities' do
+    before do
+      allow_any_instance_of(Poplidays::Importer).to receive(:fetch_property_details) { Result.new(property_details) }
+      allow_any_instance_of(Poplidays::Importer).to receive(:fetch_availabilities) { Result.error(:timeout_error) }
+    end
+
+    it 'announces an error if fetching description fails' do
+      subject.perform
+
+      error = ExternalErrorRepository.last
+
+      expect(error.operation).to eq 'sync'
+      expect(error.supplier).to eq Poplidays::Client::SUPPLIER_NAME
+      expect(error.code).to eq 'timeout_error'
+    end
+
+    it 'doesnt finalize synchronisation with external error' do
+      expect(Roomorama::Client::Operations).to_not receive(:disable)
+      subject.perform
+    end
+  end
+
+  context 'success' do
+
+    before do
+      allow_any_instance_of(Poplidays::Importer).to receive(:fetch_property_details) { Result.new(property_details) }
+      allow_any_instance_of(Poplidays::Importer).to receive(:fetch_availabilities) { Result.new(availabilities) }
+    end
+
+    it 'finalizes synchronisation' do
+      allow_any_instance_of(Roomorama::Client).to receive(:perform) { Result.new('success') }
+
+      expect(subject.synchronisation).to receive(:finish!)
+      subject.perform
+    end
+  end
+
+  def parse_json(json_string)
+    Yajl::Parser.parse(json_string)
+  end
+end
---
+++ spec/lib/concierge/suppliers/poplidays/mappers/roomorama_calendar_spec.rb
@@ -0,0 +1,200 @@
+require 'spec_helper'
+
+RSpec.describe Poplidays::Mappers::RoomoramaCalendar do
+  include Support::Fixtures
+
+  let(:property_id) { '8927439190' }
+  let(:today) { Date.new(2016, 7, 14) }
+  let(:property_details) { {'mandatoryServicesPrice' => 25.0} }
+
+  subject { described_class.new }
+
+  before do
+    allow(Date).to receive(:today).and_return(today)
+  end
+
+  describe '#build' do
+    it 'returns roomorama calendar' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160831',
+            'basePrice' => 960.0,
+            'departure' => '20160924',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+
+      expect { calendar.validate! }.to_not raise_error
+      expect(calendar).to be_a(Roomorama::Calendar)
+      expect(calendar.property_identifier).to eq(property_id)
+    end
+
+    it 'returns unavailable entry for on request stays' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160831',
+            'basePrice' => 960.0,
+            'departure' => '20160924',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => true
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 11)}
+      expect(entry.available).to be_falsey
+    end
+
+    it 'returns unavailable entry for price disabled stays' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160831',
+            'basePrice' => 960.0,
+            'departure' => '20160924',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => false,
+            'requestOnly' => false
+          }
+        ]
+      }
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 11)}
+      expect(entry.available).to be_falsey
+    end
+
+    it 'correctly fills checkin/checkout allowed' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160831',
+            'basePrice' => 960.0,
+            'departure' => '20160924',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      start_entry = calendar.entries.detect { |e| e.date == Date.new(2016, 8, 31)}
+      expect(start_entry.available).to be_truthy
+      expect(start_entry.checkin_allowed).to be_truthy
+      expect(start_entry.checkout_allowed).to be_falsey
+
+      middle_entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 11)}
+      expect(middle_entry.available).to be_truthy
+      expect(middle_entry.checkin_allowed).to be_falsey
+      expect(middle_entry.checkout_allowed).to be_falsey
+
+      end_entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 24)}
+      expect(end_entry.available).to be_truthy
+      expect(end_entry.checkin_allowed).to be_falsey
+      expect(end_entry.checkout_allowed).to be_truthy
+    end
+
+    it 'returns unavailable entry for dates outside of availabilities' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160831',
+            'basePrice' => 960.0,
+            'departure' => '20160924',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      entry = calendar.entries.detect { |e| e.date == Date.new(2016, 7, 20)}
+      expect(entry.available).to be_falsey
+    end
+
+    it 'calculates nigthly rate' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160901',
+            'basePrice' => 960.0,
+            'departure' => '20160913',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          },
+          {
+            'arrival' => '20160901',
+            'basePrice' => 1500.0,
+            'departure' => '20160926',
+            'discountPercent' => 0.0,
+            'price' => 1500.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 11)}
+      expect(entry.nightly_rate).to eq(61.0)
+    end
+
+    it 'fills minimum_stay for start date of each stay' do
+      availabilities_hash = {
+        'availabilities' => [
+          {
+            'arrival' => '20160901',
+            'basePrice' => 960.0,
+            'departure' => '20160913',
+            'discountPercent' => 0.0,
+            'price' => 960.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          },
+          {
+            'arrival' => '20160901',
+            'basePrice' => 860.0,
+            'departure' => '20160910',
+            'discountPercent' => 0.0,
+            'price' => 860.0,
+            'priceEnabled' => true,
+            'requestOnly' => false
+          }
+        ]
+      }
+
+      calendar = subject.build(property_id, property_details, availabilities_hash)
+      expect { calendar.validate! }.to_not raise_error
+
+      entry = calendar.entries.detect { |e| e.date == Date.new(2016, 9, 1)}
+      expect(entry.minimum_stay).to eq(9)
+    end
+  end
+end
