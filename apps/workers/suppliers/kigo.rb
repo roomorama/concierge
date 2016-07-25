f031d7ed25ce7e8bca4a16ca4e031c4b76b085bb
igor <igortr333@gmail.com>
2016-07-25 15:04:38 +1000

    fix worker

--- apps/workers/suppliers/kigo.rb
+++ apps/workers/suppliers/kigo.rb
@@ -1,72 +1,90 @@
-module Workers::Suppliers
-  # +Workers::Suppliers::Kigo+
-  #
-  # Performs synchronisation with supplier
-  class Kigo
-    SUPPLIER_NAME = 'Kigo'
+# +Workers::Suppliers::Kigo+
+#
+# Performs synchronisation with supplier
+class Workers::Suppliers::Kigo
 
-    attr_reader :synchronisation, :host
+  SUPPLIER_NAME = 'Kigo'
 
-    def initialize(host)
-      @host            = host
-      @synchronisation = Workers::Synchronisation.new(host)
-    end
+  attr_reader :synchronisation, :host
 
-    def perform
-      result = importer.fetch_properties
-      if result.success?
-        properties = result.value
-        properties.each do |property|
-          id = property['PROP_ID']
-          synchronisation.start(id) do
-            result = importer.fetch_data(id)
-            if result.success?
-              mapper.prepare(result.value)
-            else
-              synchronisation.failed!
-              message = "Failed to perform the `#fetch_data` operation, with identifier: `#{id}`"
-              announce_error(message, result)
-            end
-          end
+  def initialize(host)
+    @host            = host
+    @synchronisation = Workers::PropertySynchronisation.new(host)
+  end
+
+  def perform
+    result = importer.fetch_properties
+    if result.success?
+      properties = result.value
+      properties.each do |property|
+        next if property['PROP_PROVIDER'].nil?
+        id     = property['PROP_ID']
+        result = importer.fetch_data(id)
+
+        if result.success?
+          mapper.prepare(result.value)
+        else
+          synchronisation.failed!
+          message = "Failed to perform the `#fetch_data` operation, with identifier: `#{id}`"
+          announce_error(message, result)
+          result
         end
-        synchronisation.finish!
-      else
-        message = "Failed to perform the `#fetch_properties` operation"
-        announce_error(message, result)
       end
-    end
-
-    private
-
-    def importer
-      @importer ||= ::Kigo::Importer.new(credentials)
-    end
-
-    def credentials
-      Concierge::Credentials.for(SUPPLIER_NAME)
-    end
-
-    def announce_error(message, result)
-      message = {
-        label: 'Synchronisation Failure',
-        message: message,
-        backtrace: caller
-      }
-      context = Concierge::Context::Message.new(message)
-      Concierge.context.augment(context)
-
-      Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
-        operation:   'sync',
-        supplier:    SUPPLIER_NAME,
-        code:        result.error.code,
-        context:     Concierge.context.to_h,
-        happened_at: Time.now
-      })
+      synchronisation.finish!
+    else
+      message = "Failed to perform the `#fetch_properties` operation"
+      announce_error(message, result)
     end
   end
+
+  private
+
+  def filter(properties)
+    properties.select do |property|
+      provider = property['PROP_PROVIDER']
+      provider && (ids.include?(provider['RA_ID'] || ids.include?(provider['OWNER_ID'])))
+    end
+
+  end
+
+
+  def importer
+    @importer ||= Kigo::Importer.new(credentials, request_handler)
+  end
+
+  def request_handler
+    Kigo::Request.new(credentials)
+  end
+
+  def mapper
+    Kigo::Mappers::Property.new(references: importer.fetch_references)
+  end
+
+  def credentials
+    Concierge::Credentials.for(SUPPLIER_NAME)
+  end
+
+  def announce_error(message, result)
+    message = {
+      label:     'Synchronisation Failure',
+      message:   message,
+      backtrace: caller
+    }
+    context = Concierge::Context::Message.new(message)
+    Concierge.context.augment(context)
+
+    Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
+      operation:   'sync',
+      supplier:    SUPPLIER_NAME,
+      code:        result.error.code,
+      context:     Concierge.context.to_h,
+      happened_at: Time.now
+    })
+  end
+
 end
 
 # listen supplier worker
-Concierge::Announcer.on("sync.Kigo") do |host|
+Concierge::Announcer.on("metadata.Kigo") do |host|
   Workers::Suppliers::Kigo.new(host).perform
 end
