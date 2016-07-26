599a802411b3bb7b5c52e33a6b8a5d5ba0909c37
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-26 17:34:17 +0200

    Add property details validation

---
+++ lib/concierge/suppliers/poplidays/validators/property_details_validator.rb
@@ -0,0 +1,34 @@
+module Poplidays
+  module Validators
+    # +Poplidays::PropertyDetailsValidator+
+    #
+    # This class responsible for property details validation.
+    # cases when property invalid:
+    #
+    #   * property is on request only
+    #   * property price is only available through call center
+    #
+    class PropertyDetailsValidator
+      attr_reader :details
+
+      # details is a hash representation of response from Poplidays lodging method
+      def initialize(details)
+        @details = details
+      end
+
+      def valid?
+        !on_request_only? && price_enabled?
+      end
+
+      private
+
+      def on_request_only?
+        details['requestOnly']
+      end
+
+      def price_enabled?
+        details['priceEnabled']
+      end
+    end
+  end
+end
