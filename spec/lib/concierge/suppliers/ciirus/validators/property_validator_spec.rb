e0b2689bcdb9dee0c419754e08f981d933a5651c
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-26 10:55:42 +0200

    Ciirus metadata sync sends property to roomorama even if it can not determine country code

--- spec/lib/concierge/suppliers/ciirus/validators/property_validator_spec.rb
+++ spec/lib/concierge/suppliers/ciirus/validators/property_validator_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Ciirus::Validators::PropertyValidator do
 
-  let(:invalid_country_property) { double(type: 'Unspecified', country: '654987')}
-  let(:invalid_type_property) { double(type: 'Unspecified', country: 'Turkey')}
-  let(:valid_property) { double(type: 'Motel', country: 'USA')}
+  let(:invalid_type_property) { double(type: 'Unspecified')}
+  let(:valid_property) { double(type: 'Motel')}
 
   describe '#valid?' do
     it 'returns true for valid property' do
@@ -14,11 +13,6 @@ RSpec.describe Ciirus::Validators::PropertyValidator do
 
     it 'returns false for property with invalid type' do
       validator = described_class.new(invalid_type_property)
-      expect(validator.valid?).to be_falsey
-    end
-
-    it 'returns false for property with invalid country' do
-      validator = described_class.new(invalid_country_property)
       expect(validator.valid?).to be_falsey
     end
   end
