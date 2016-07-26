ca7ed837faa61bba674330403682342f51c2bc11
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-07-26 10:21:01 +0600

    Woori: importer specs

--- spec/lib/concierge/suppliers/woori/importer_spec.rb
+++ spec/lib/concierge/suppliers/woori/importer_spec.rb
@@ -1,6 +1,47 @@
 require 'spec_helper'
 
 RSpec.describe Woori::Importer do
+  include Support::HTTPStubbing
+  include Support::Fixtures
 
+  let(:credentials) { Concierge::Credentials.for("Woori") }
+  let(:importer) { described_class.new(credentials) }
 
+  describe "#fetch_properties" do
+    it "calls fetcher class to load properties" do
+      fetcher_class = Woori::Commands::PropertiesFetcher
+      updated_at = "1970-01-01"
+      limit = 50
+      offset = 0
+
+      expect_any_instance_of(fetcher_class).to(
+        receive(:call).with(updated_at, limit, offset)
+      )
+      importer.fetch_properties(updated_at, limit, offset)
+    end
+  end
+
+  describe "#fetch_units" do
+    it "calls fetcher class to load properties" do
+      fetcher_class = Woori::Commands::UnitsFetcher
+      property_id = "abcd"
+
+      expect_any_instance_of(fetcher_class).to(
+        receive(:call).with(property_id)
+      )
+      importer.fetch_units(property_id)
+    end
+  end
+
+  describe "#fetch_unit_rates" do
+    it "calls fetcher class to load properties" do
+      fetcher_class = Woori::Commands::UnitRatesFetcher
+      unit_id = "abcd"
+
+      expect_any_instance_of(fetcher_class).to(
+        receive(:call).with(unit_id)
+      )
+      importer.fetch_unit_rates(unit_id)
+    end
+  end
 end
