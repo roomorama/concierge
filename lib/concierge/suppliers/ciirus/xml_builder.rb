56318b7c4c708bb37078ea90735d6a643c3daa2c
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-30 00:23:10 +0200

    Remove useless cleaning fee fetcher, because this information is not needed for CiiRUS supplier

--- lib/concierge/suppliers/ciirus/xml_builder.rb
+++ lib/concierge/suppliers/ciirus/xml_builder.rb
@@ -126,17 +126,6 @@ module Ciirus
       message.doc.root.children.to_xml
     end
 
-    def cleaning_fee(property_id)
-      message = builder.new do |xml|
-        xml.root do
-          xml.APIUserName credentials.username
-          xml.APIPassword credentials.password
-          xml.PropertyID property_id
-        end
-      end
-      message.doc.root.children.to_xml
-    end
-
     def extras(property_id)
       message = builder.new do |xml|
         xml.root do
