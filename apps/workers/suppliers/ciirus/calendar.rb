f0bc08095d148e850a757f4726a1ed30b3fb8d70
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-07-31 07:45:16 +0200

    Remove unused method

--- apps/workers/suppliers/ciirus/calendar.rb
+++ apps/workers/suppliers/ciirus/calendar.rb
@@ -91,18 +91,6 @@ module Workers::Suppliers::Ciirus
       context = Concierge::Context::Message.new(message)
       Concierge.context.augment(context)
     end
-
-    def announce_error(message, result)
-      augment_context_error(message)
-
-      Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
-        operation:   'sync',
-        supplier:    Ciirus::Client::SUPPLIER_NAME,
-        code:        result.error.code,
-        context:     Concierge.context.to_h,
-        happened_at: Time.now
-      })
-    end
   end
 end
 
