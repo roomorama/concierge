095c13a78f10bf461f7ced86a2cd6296b550fd1e
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-02 15:24:23 +0600

    Woori: update client specs with spec which expects Commands::Cancel call

--- spec/lib/concierge/suppliers/woori/client_spec.rb
+++ spec/lib/concierge/suppliers/woori/client_spec.rb
@@ -3,14 +3,26 @@ require 'spec_helper'
 RSpec.describe Woori::Client do
   let(:credentials) { Concierge::Credentials.for("Woori") }
   let(:client) { described_class.new(credentials) }
-  let(:params) { {} }
 
   describe "#quote" do
+    let(:params) { {} }
+
     it "calls quotation fetcher class" do
       fetcher_class = Woori::Commands::QuotationFetcher
 
       expect_any_instance_of(fetcher_class).to(receive(:call).with(params))
       client.quote(params)
+    end
+  end
+
+  describe "#cancel" do
+    let(:params) { { reservation_id: '123' } }
+
+    it "calls cancel command class" do
+      fetcher_class = Woori::Commands::Cancel
+
+      expect_any_instance_of(fetcher_class).to(receive(:call).with('123'))
+      client.cancel(params)
     end
   end
 end
