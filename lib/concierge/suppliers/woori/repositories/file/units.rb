1cf5e2cb3feb63aec7b8655a1b6e63e8604caa33
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-03 16:11:05 +0600

    Woori: create file & http repositories to fetch properties from

---
+++ lib/concierge/suppliers/woori/repositories/file/units.rb
@@ -0,0 +1,45 @@
+module Woori::Repositories::File
+  class Units < Base
+    attr_reader :files
+
+    def initialize(files_path)
+      @files = files_path.map { |file_path| File.new(file_path, 'r') }
+    end
+
+    def all
+      @units ||= raw_units.map do |unit_hash|
+        safe_hash = Concierge::SafeAccessHash.new(unit_hash)
+        mapper = Woori::Mappers::RoomoramaUnit.new(safe_hash)
+        mapper.build_unit
+      end
+    end
+
+    def find(unit_id)
+      all.find { |u| u.identifier == unit_id }
+    end
+
+    def find_all_by_property_id(property_id)
+      all.select { |u| u.identifier.start_with?(property_id) }
+    end
+
+    private
+
+    def raw_units
+      items = []
+
+      files.each do |file|
+        decoded_result = json_decode(file)
+        
+        units_data = decoded_result.value["data"]
+        return [] unless units_data
+
+        file_items = units_data["items"]
+        return [] unless file_items
+
+        items = items + file_items
+      end
+
+      items
+    end
+  end
+end
--- spec/lib/concierge/suppliers/woori/repositories/http/units_spec.rb
+++ spec/lib/concierge/suppliers/woori/repositories/http/units_spec.rb
@@ -1,42 +1,57 @@
 require "spec_helper"
 
-RSpec.describe Woori::Commands::UnitRatesFetcher do
+RSpec.describe Woori::Repositories::HTTP::Units do
   include Support::HTTPStubbing
   include Support::Fixtures
   include Support::Woori::LastContextEvent
 
   let(:credentials) { Concierge::Credentials.for("Woori") }
   let(:subject) { described_class.new(credentials) }
-  let(:unit_id) { "w_w0104006_R01" }
-  let(:url) { "http://my.test/available" }
+  let(:property_id) { "w_w0104006" }
+  let(:url) { "http://my.test/properties/#{property_id}/roomtypes" }
 
-  it "returns result wrapping a unit rates object" do
-    stub_data = read_fixture("woori/unit_rates/success.json")
+  it "returns results with an array of units" do
+    stub_data = read_fixture("woori/units/success.json")
     stub_call(:get, url) { [200, {}, stub_data] }
 
-    result = subject.call(unit_id)
+    result = subject.find_all_by_property_id(property_id)
     expect(result.success?).to be true
 
-    unit_rates = result.value
-    expect(unit_rates).to be_kind_of(Woori::Entities::UnitRates)
+    properties = result.value
+
+    expect(properties.size).to eq(9)
+    expect(properties).to all(be_kind_of(Roomorama::Unit))
   end
   
-  it "returns nil when there is no rates information" do
-    stub_data = read_fixture("woori/unit_rates/empty.json")
+  it "returns results when there is only one unit" do
+    stub_data = read_fixture("woori/units/one.json")
     stub_call(:get, url) { [200, {}, stub_data] }
 
-    result = subject.call(unit_id)
+    result = subject.find_all_by_property_id(property_id)
     expect(result.success?).to be true
 
-    unit_rates = result.value
-    expect(unit_rates).to be_nil
+    properties = result.value
+
+    expect(properties.size).to eq(1)
+    expect(properties).to all(be_kind_of(Roomorama::Unit))
+  end
+
+  it "returns an empty array when there is no units" do
+    stub_data = read_fixture("woori/units/empty.json")
+    stub_call(:get, url) { [200, {}, stub_data] }
+
+    result = subject.find_all_by_property_id(property_id)
+    expect(result.success?).to be true
+
+    properties = result.value
+    expect(properties.size).to eq(0)
   end
 
   it "returns failure result when Woori API returns an error" do
     stub_data = read_fixture("woori/error_500.json")
     stub_call(:get, url) { [500, {}, stub_data] }
 
-    result = subject.call(unit_id)
+    result = subject.find_all_by_property_id(property_id)
 
     expect(result.success?).to be false
     expect(result.error.code).to eq(:http_status_500)
@@ -47,7 +62,7 @@ RSpec.describe Woori::Commands::UnitRatesFetcher do
       stub_data = read_fixture("woori/bad_response.json")
       stub_call(:get, url) { [200, {}, stub_data] }
 
-      result = subject.call(unit_id)
+      result = subject.find_all_by_property_id(property_id)
 
       expect(result.success?).to be false
       expect(result.error.code).to eq(:invalid_json_representation)
@@ -58,7 +73,7 @@ RSpec.describe Woori::Commands::UnitRatesFetcher do
     it "returns a result with an appropriate error" do
       stub_call(:get, url) { raise Faraday::TimeoutError }
 
-      result = subject.call(unit_id)
+      result = subject.find_all_by_property_id(property_id)
 
       expect(result).not_to be_success
       expect(last_context_event[:message]).to eq("timeout")
