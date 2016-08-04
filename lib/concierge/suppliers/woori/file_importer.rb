90025122441b4ef5cc7091b20d004717ff50fe96
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-04 09:54:57 +0600

    Woori: file_importer documentation update

--- lib/concierge/suppliers/woori/file_importer.rb
+++ lib/concierge/suppliers/woori/file_importer.rb
@@ -1,19 +1,32 @@
 module Woori
-  # +Woori::Importer+
+  # +Woori::FileImporter+
   #
   # This class provides an interface for the bulk import of Woori properties
-  # from .json files
+  # +Woori::FileImporter+ imports data from files (from .json files)
   class FileImporter
+    # Retrieves the list of all properties
+    #
+    # Returns an +Array+ of +Roomorama::Property+ objects
     def fetch_all_properties
       repository = Repositories::File::Properties.new(properties_file)
       repository.all
     end
 
+    # Retrieves the list of all units
+    #
+    # Returns an +Array+ of +Roomorama::Unit+ objects
     def fetch_all_units
       repository = Repositories::File::Units.new(unit_files)
       repository.all
     end
 
+    # Retrieves the list of units for a given property by its id
+    #
+    # Arguments:
+    #
+    #   * +property_id+ [String] property id (property hash in Woori API)
+    #
+    # Returns an +Array+ of +Roomorama::Unit+ objects
     def fetch_all_property_units(property_id)
       repository = Repositories::File::Units.new(unit_files)
       repository.find_all_by_property_id(property_id)
