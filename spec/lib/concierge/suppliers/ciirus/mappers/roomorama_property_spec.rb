b38000a3961a2e1ee5745f78da910e2a89f73693
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-08-05 11:27:53 +0200

    Change cancellation policy

--- spec/lib/concierge/suppliers/ciirus/mappers/roomorama_property_spec.rb
+++ spec/lib/concierge/suppliers/ciirus/mappers/roomorama_property_spec.rb
@@ -97,7 +97,7 @@ RSpec.describe Ciirus::Mappers::RoomoramaProperty do
     expect(roomorama_property.amenities).to eq(amenities)
     expect(roomorama_property.pets_allowed).to be(true)
     expect(roomorama_property.currency).to eq('USD')
-    expect(roomorama_property.cancellation_policy).to eq('moderate')
+    expect(roomorama_property.cancellation_policy).to eq('super_elite')
 
     expect(roomorama_property.images.length).to eq(1)
     image = roomorama_property.images.first
