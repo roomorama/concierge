687e218ebe1d2a7d1d1313a91e1af25f2de8a740
Renato Mascarenhas <renato@roomorama.com>
2016-08-05 14:01:04 +0800

    Silence Rake task during specs.
    The feedback provided by the Rake task was getting mixed with regular
    RSpec syntax.

--- spec/lib/tasks/properties.rake_spec.rb
+++ spec/lib/tasks/properties.rake_spec.rb
@@ -2,12 +2,21 @@ require 'spec_helper'
 
 RSpec.describe "properties rake tasks" do
   include Support::Factories
+  let!(:old_stdout) { $stdout.dup }
 
   before do
-    @rake = Rake::Application.new
-    Rake.application = @rake
+    Rake.application = Rake::Application.new
     load Hanami.root.join('Rakefile')
     Rake::Task.define_task(:environment)
+
+    # redirect standard output to +/dev/null+ so that feedback produced by the
+    # Rake task does not get mingled with RSpec's output. The +after+ block is
+    # responsible for setting +stdout+ back to its original value.
+    $stdout.reopen("/dev/null")
+  end
+
+  after do
+    $stdout.reopen(old_stdout)
   end
 
   let(:refs) { ["001", "002"] }
