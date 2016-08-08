13ca93932a44cc2b8b069cde4acb800f0e4813a9
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-08 17:00:21 +0600

    Woori: worker updates, add number_of_units

--- apps/workers/suppliers/woori/file_metadata.rb
+++ apps/workers/suppliers/woori/file_metadata.rb
@@ -6,6 +6,8 @@ module Workers::Suppliers::Woori
   # There is no event listener for this worker, synchronisation should be 
   # started manually.
   class FileMetadata
+    class UnitRatesFetchError < StandardError; end
+
     attr_reader :synchronisation, :host
     
     def initialize(host)
@@ -14,24 +16,35 @@ module Workers::Suppliers::Woori
     end
 
     def perform
+      puts "Parsing properties..."
       all_properties.each do |property|
         synchronisation.start(property.identifier) do
+          puts "Starting sync for #{property.identifier}"
           Concierge.context.disable!
 
           units = all_property_units(property.identifier)
+          puts "  Found #{units.size} units for #{property.identifier}"
           units.each do |unit|
+            puts "    Fetching rates for #{unit.identifier}..."
+            rates_result = fetch_unit_rates(unit)
 
-            unit.number_of_units = 1
-            unit.nightly_rate = 10000.0
-            unit.weekly_rate = 70000.0
-            unit.monthly_rate = 300000.0
+            if rates_result.success?
+              rates = rates_result.value
+
+              unit.nightly_rate = rates.nightly_rate
+              unit.weekly_rate  = rates.weekly_rate
+              unit.monthly_rate = rates.monthly_rate
+            else
+              announce_property_unit_rates_fetch_error(unit, rates_result)
+              rates_result
+            end
 
             property.add_unit(unit)
           end
 
-          property.nightly_rate = 10000.0
-          property.weekly_rate = 70000.0
-          property.monthly_rate = 300000.0
+          property.nightly_rate = units.map(&:nightly_rate).max
+          property.weekly_rate = units.map(&:weekly_rate).max
+          property.monthly_rate = units.map(&:monthly_rate).max
 
           property.type = 'apartment'
           Result.new(property)
@@ -47,12 +60,63 @@ module Workers::Suppliers::Woori
       @file_importer ||= ::Woori::FileImporter.new
     end
 
+    def api_importer
+      @api_importer ||= ::Woori::Importer.new(credentials)
+    end
+
+    def credentials
+      Concierge::Credentials.for(::Woori::Client::SUPPLIER_NAME)
+    end
+
     def all_properties
       @all_properties ||= file_importer.fetch_all_properties
     end
 
     def all_property_units(property_id)
       @all_units ||= file_importer.fetch_all_property_units(property_id)
+    end
+
+    def fetch_unit_rates(unit)
+      retries ||= 3
+      result = api_importer.fetch_unit_rates(unit.identifier)
+
+      if result.success?
+        result
+      else
+        raise UnitRatesFetchError
+      end
+    rescue UnitRatesFetchError
+      if (retries -= 1) > 0
+        puts "Failed to fetch rates for #{unit.identifier}. Retrying..."
+        retry
+      else
+        puts "No more retries for #{unit.identifier}"
+        result
+      end
+    end
+
+    def announce_property_unit_rates_fetch_error(unit, result)
+      message = "Failed to perform the `#fetch_unit_rates` operation for unit id=#{unit.identifier}"
+
+      announce_error(message, result)
+    end
+
+    def announce_error(message, result)
+      message = {
+        label: 'Synchronisation Failure',
+        message: message,
+        backtrace: caller
+      }
+      context = Concierge::Context::Message.new(message)
+      Concierge.context.augment(context)
+
+      Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
+        operation:   'sync',
+        supplier:    ::Woori::Client::SUPPLIER_NAME,
+        code:        result.error.code,
+        context:     Concierge.context.to_h,
+        happened_at: Time.now
+      })
     end
   end
 end
