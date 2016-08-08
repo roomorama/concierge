41543efa003baef21549a3fa3dd7989ad470acd3
Igor Trofimov <igor@roomorama.com>
2016-08-08 10:07:48 +0800

    remove strange db method

--- lib/concierge/suppliers/kigo/legacy.rb
+++ lib/concierge/suppliers/kigo/legacy.rb
@@ -33,21 +33,17 @@ module Kigo
     # Returns a +Result+ wrapping a +Quotation+ when operation succeeds
     # Returns a +Result+ wrapping a nil object when operation fails
     def quote(params)
-      Kigo::Price.new(credentials, request_handler: legacy_handler).quote(params)
+      Kigo::Price.new(credentials, request_handler: request_handler).quote(params)
     end
 
     # Returns a +Result+ wrapping a +Reservation+.
     # Returns a +Result+ with error if booking fails.
     # Uses an instance +Kigo::LegacyRequest+ to dictate parameters and endpoints.
     def book(params)
-      Kigo::Booking.new(credentials, request_handler: legacy_handler).book(params)
+      Kigo::Booking.new(credentials, request_handler: request_handler).book(params)
     end
 
     private
-
-    def database
-      @database ||= Concierge::OptionalDatabaseAccess.new(ReservationRepository)
-    end
 
     def request_handler
       LegacyRequest.new(credentials)
