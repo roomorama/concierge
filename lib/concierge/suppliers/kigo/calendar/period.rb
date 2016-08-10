22ce854558a84a497c4871fe1f516845e7eeea7a
Igor Trofimov <igor@roomorama.com>
2016-08-10 10:43:01 +0800

    fix period, add tests

--- lib/concierge/suppliers/kigo/calendar/period.rb
+++ lib/concierge/suppliers/kigo/calendar/period.rb
@@ -1,4 +1,4 @@
-module Kigo::Calendar
+class Kigo::Calendar
 
   TimeInterval = Struct.new(:interval) do
     # returns days count computed by NIGHT, MONTH, YEAR unit
@@ -15,20 +15,23 @@ module Kigo::Calendar
     attr_reader :start_date, :end_date, :min_stay, :amounts
 
     def initialize(period)
-      @start_date = Date.parse(period['CHECK_IN'])
-      @end_date   = Date.parse(period['CHECK_OUT'])
+      start_date = Date.parse(period['CHECK_IN'])
+      end_date   = Date.parse(period['CHECK_OUT'])
+
+      @start_date = define_start_date(start_date)
+      @end_date   = define_end_date(end_date)
       @min_stay   = period['STAY_MIN']
       @amounts    = period['NIGHTLY_AMOUNTS']
     end
 
     def entries
-      start_date..end_date.map do |date|
+      (start_date..end_date).map do |date|
         build_entry(date)
       end
     end
 
     def valid?
-      valid_range.include?(start_date..end_date)
+      valid_range.include?(start_date) && valid_range.include?(end_date)
     end
 
     private
@@ -46,7 +49,7 @@ module Kigo::Calendar
     end
 
     def nightly_rate
-      amounts.min_by { |amount| amount['amount'].to_f }
+      amounts.map { |amount| amount['AMOUNT'].to_f }.min
     end
 
     def minimum_stay
@@ -54,8 +57,23 @@ module Kigo::Calendar
     end
 
     def valid_range
-      year_from_today = Date.today + 365
-      Date.today..year_from_today
+      today..year_from_today
+    end
+
+    def define_start_date(date)
+      date < today ? today : date
+    end
+
+    def define_end_date(date)
+      date > year_from_today ? year_from_today : date
+    end
+
+    def today
+      Date.today
+    end
+
+    def year_from_today
+      today + 365
     end
   end
 end
