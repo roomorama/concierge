eae447dd1d5b9d45f402635d129387557e9a7a42
Igor Trofimov <igor@roomorama.com>
2016-08-10 14:04:27 +0800

    move TimeInterval struct

--- lib/concierge/suppliers/kigo/calendar.rb
+++ lib/concierge/suppliers/kigo/calendar.rb
@@ -1,5 +1,20 @@
 module Kigo
+  # +Kigo::TimeInterval+
+  #
+  # represents days count according to +UNIT+ type
+  # possible unit types NIGHT, MONTH, YEAR
+  TimeInterval = Struct.new(:interval) do
+    def days
+      return 1 if interval['NUMBER'].zero?
+      multiplier = { 'MONTH' => 30, 'YEAR' => 365 }.fetch(interval['UNIT'], 1)
+      interval['NUMBER'] * multiplier
+    end
+  end
 
+  #  +Kigo::Calendar+
+  #
+  # responsible for performing date periods and availabilities list
+  # to +Roomorama::Calendar+ instance
   class Calendar
 
     attr_reader :property_identifier, :entries
@@ -11,7 +26,8 @@ module Kigo
 
     def perform(pricing, availabilities)
       process_periods(pricing)
-      # updated_entries = set_availabilities(availabilities)
+      set_availabilities(availabilities)
+
       entries.each do |entry|
         calendar.add(entry)
       end
