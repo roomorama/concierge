caef358e839e1a6c12e398a454d5fba299caa5d7
Keang <skeang@gmail.com>
2016-08-12 10:45:00 +0800

    Add external error scoping

--- apps/web/controllers/external_errors/index.rb
+++ apps/web/controllers/external_errors/index.rb
@@ -7,14 +7,19 @@ module Web::Controllers::ExternalErrors
     include Web::Controllers::InternalError
 
     params Web::Controllers::Params::Paginated
+    params do
+      param :supplier, type: String
+    end
 
     expose :external_errors
 
     def call(params)
-      page = params[:page] && params[:page].to_i
-      per  = params[:per]  && params[:per].to_i
+      page     = params[:page] && params[:page].to_i
+      per      = params[:per]  && params[:per].to_i
+      supplier = params[:supplier]
 
-      @external_errors = ExternalErrorRepository.reverse_occurrence.paginate(page: page, per: per)
+      @external_errors = ExternalErrorRepository.from_supplier(supplier).
+                           reverse_occurrence.paginate(page: page, per: per)
     end
   end
 end
