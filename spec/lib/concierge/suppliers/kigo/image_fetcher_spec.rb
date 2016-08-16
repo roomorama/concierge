31ea4584dfe1e088c7a9e20c74ebfea84cc90f96
Igor Trofimov <igor@roomorama.com>
2016-08-16 10:26:37 +0800

    fix tests

--- spec/lib/concierge/suppliers/kigo/image_fetcher_spec.rb
+++ spec/lib/concierge/suppliers/kigo/image_fetcher_spec.rb
@@ -2,25 +2,30 @@ require 'spec_helper'
 
 RSpec.describe Kigo::ImageFetcher do
 
-  let(:image_path) { './some/path'}
-  subject { described_class.new(image_path) }
-
   describe '#fetch' do
-    let(:property_id) { 1 }
-    let(:identifier) { 'hashed-string' }
+    let(:params) {
+      { property_id: 123, image_id: '123' }
+    }
     let(:response) { 'base64-encoded-string' }
     let(:decoded_response) { Base64.decode64(response) }
 
     it 'saves image from decoded base64 response' do
       allow_any_instance_of(Kigo::Importer).to receive(:fetch_image) { Result.new(response) }
 
-      file = double('file')
-      image_url = [image_path, "#{identifier}.jpg"]
-      expect(File).to receive(:open).with(image_url, 'w').and_yield(file)
-      expect(file).to receive(:write).with(decoded_response)
-      subject.fetch(property_id, identifier)
+      result = subject.fetch(params)
 
-      expect(subject.path).to eq image_path
+      expect(result).to be_success
+      expect(result.value).to be_a(StringIO)
+    end
+
+    it 'creates an error with failed request' do
+      allow_any_instance_of(Kigo::Importer).to receive(:fetch_image) { Result.error(:connection_timeout) }
+      result = nil
+
+      expect { result = subject.fetch(params) }.to change { ExternalErrorRepository.count }
+
+      expect(result).not_to be_success
+      expect(result.error.code).to eq :connection_timeout
     end
   end
 
