1b00a27f90d03c0f5a609b38b3cd4f35574f23bf
Igor Trofimov <igor@roomorama.com>
2016-08-16 10:47:55 +0800

    add more valuable error message

--- lib/concierge/suppliers/kigo/importer.rb
+++ lib/concierge/suppliers/kigo/importer.rb
@@ -101,10 +101,10 @@ module Kigo
 
       return payload unless payload.success?
 
-      if payload.value['API_RESULT_CODE'] == 'E_OK'
-        Result.new(payload.value['API_REPLY'])
-      else
-        Result.error(:invalid_request)
+      case payload.value['API_RESULT_CODE']
+        when 'E_OK'     then Result.new(payload.value['API_REPLY'])
+        when 'E_NOSUCH' then Result.error(:invalid_input_data)
+        when 'E_INPUT'  then Result.error(:record_not_found)
       end
     end
 
