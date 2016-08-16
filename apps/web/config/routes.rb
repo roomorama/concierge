6fbc24960e29d3aa0c687b45b5bbc3f221c5e255
Igor Trofimov <igor@roomorama.com>
2016-08-16 09:55:18 +0800

    add image controller

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -4,3 +4,5 @@ resources :errors,         only: [:index, :show], controller: "external_errors"
 resources :reservations,   only: [:index]
 resources :suppliers,      only: [:show]
 resources :sync_processes, only: [:index]
+
+get "/kigo_image/:property_id/:image_id", to: "kigo_image#show"
