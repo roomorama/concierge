db71ae14039972c9860af80f3dc27b6c2a26ec1c
Keang <skeang@gmail.com>
2016-08-22 18:20:28 +0800

    Remove context init

--- apps/workers/suppliers/ciirus/metadata.rb
+++ apps/workers/suppliers/ciirus/metadata.rb
@@ -11,7 +11,6 @@ module Workers::Suppliers::Ciirus
     end
 
     def perform
-      initialize_overall_sync_context
       result = importer.fetch_properties(host)
 
       if result.success?
@@ -157,16 +156,6 @@ module Workers::Suppliers::Ciirus
         context:     Concierge.context.to_h,
         happened_at: Time.now
       })
-    end
-
-    def initialize_overall_sync_context
-      Concierge.context = Concierge::Context.new(type: "batch")
-      sync_process = Concierge::Context::SyncProcess.new(
-        worker:     Workers::PropertySynchronisation::WORKER_TYPE,
-        host_id:    host.id,
-        identifier: nil
-      )
-      Concierge.context.augment(sync_process)
     end
   end
 end
