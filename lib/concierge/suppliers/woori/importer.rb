01e5dfdde30095d6ade6e5b8cff932c1b2e569f9
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-23 16:31:24 +0600

    Woori: remove not-needed code for fetching properties and units from api

--- lib/concierge/suppliers/woori/importer.rb
+++ lib/concierge/suppliers/woori/importer.rb
@@ -7,52 +7,12 @@ module Woori
   # Usage
   #
   #   importer = Woori::Importer.new(credentials)
-  #   importer.fetch_properties(updated_at, limit, offset)
-  #   importer.fetch_all_property_units("w_w0104006")
   #   importer.fetch_unit_rates("w_w0104006_R01")
   class Importer
     attr_reader :credentials
 
     def initialize(credentials)
       @credentials = credentials
-    end
-
-    # Retrieves the list of properties by given options
-    #
-    # Arguments:
-    #
-    #   * +updated_at+ [String] date to start fetching properties from
-    #   * +limit+ [Integer] max number of returned properties (batch size)
-    #   * +offset+ [Integer] skip first +offset+ properties
-    #
-    # Usage:
-    #
-    #   importer.fetch_properties("1970-01-01", 50, 0)
-    #
-    # Returns a +Result+ wrapping an +Array+ of +Roomorama::Property+ objects
-    # when operation succeeds
-    # Returns a +Result+ with +Result::Error+ when operation fails
-    def fetch_properties(updated_at, limit, offset)
-      repository = Repositories::HTTP::Properties.new(credentials)
-      repository.load_updates(updated_at, limit, offset)
-    end
-
-    # Retrieves the list of units for property by its id
-    #
-    # Arguments:
-    #
-    #   * +property_id+ [String] property id (property hash in Woori API)
-    #
-    # Usage:
-    #
-    #   importer.fetch_all_property_units("w_w0104006")
-    #
-    # Returns a +Result+ wrapping an +Array+ of +Roomorama::Unit+ objects
-    # when operation succeeds
-    # Returns a +Result+ with +Result::Error+ when operation fails
-    def fetch_all_property_units(property_id)
-      repository = Repositories::HTTP::Units.new(credentials)
-      repository.find_all_by_property_id(property_id)
     end
 
     # Retrieves rates for unit by unit_id
