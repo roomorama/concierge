aa899f80e809b8b7b09a6312c8f267aa8c4efafb
Igor Trofimov <igor@roomorama.com>
2016-08-23 10:45:22 +0800

    move announcing error to controller level

--- lib/concierge/suppliers/kigo/image_fetcher.rb
+++ lib/concierge/suppliers/kigo/image_fetcher.rb
@@ -13,7 +13,6 @@ module Kigo
       if result.success?
         file_from_base64(result.value)
       else
-        announce_error(result)
         result
       end
     end
@@ -37,22 +36,5 @@ module Kigo
       Result.new(file)
     end
 
-    def announce_error(result)
-      message = {
-        label:     'Kigo image fetching failure',
-        message:   'failed to perform `#fetch_images` operation',
-        backtrace: caller
-      }
-      context = Concierge::Context::Message.new(message)
-      Concierge.context.augment(context)
-
-      Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
-        operation:   'sync',
-        supplier:    Kigo::Legacy::SUPPLIER_NAME,
-        code:        result.error.code,
-        context:     Concierge.context.to_h,
-        happened_at: Time.now
-      })
-    end
   end
 end
--- spec/lib/concierge/suppliers/kigo/image_fetcher_spec.rb
+++ spec/lib/concierge/suppliers/kigo/image_fetcher_spec.rb
@@ -20,9 +20,7 @@ RSpec.describe Kigo::ImageFetcher do
 
     it 'creates an error with failed request' do
       allow_any_instance_of(Kigo::Importer).to receive(:fetch_image) { Result.error(:connection_timeout) }
-      result = nil
-
-      expect { result = subject.fetch }.to change { ExternalErrorRepository.count }
+      result = subject.fetch
 
       expect(result).not_to be_success
       expect(result.error.code).to eq :connection_timeout
