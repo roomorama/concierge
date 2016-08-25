38d46eb54cb35cb5412cf499193e1e6067ed15f8
Igor Trofimov <igor@roomorama.com>
2016-08-25 11:09:31 +0800

    restructurize kigo workers

---
+++ apps/workers/suppliers/kigo/calendar.rb
@@ -0,0 +1,62 @@
+module Workers::Suppliers::Kigo
+  # +Workers::Suppliers::Kigo::Calendar+
+  #
+  # Performs updating properties calendar
+  class Calendar
+
+    attr_reader :synchronisation, :host, :identifiers
+
+    def initialize(host, identifiers = [])
+      @host            = host
+      @identifiers     = identifiers
+      @synchronisation = Workers::CalendarSynchronisation.new(host)
+    end
+
+    def perform
+      properties.each do |property|
+        id = property.identifier
+        synchronisation.start(id) do
+          pricing = importer.fetch_prices(id)
+          next pricing unless pricing.success?
+
+          availabilities = importer.fetch_availabilities(id)
+          next availabilities unless availabilities.success?
+
+          calendar = Kigo::Calendar.new(property)
+          calendar.perform(pricing.value, availabilities.value)
+        end
+      end
+
+      synchronisation.finish!
+    end
+
+    private
+
+    def properties
+      properties = PropertyRepository.from_host(host)
+      properties.identified_by(identifiers) if identifiers.any?
+      properties
+    end
+
+    def importer
+      @importer ||= Kigo::Importer.new(credentials, request_handler)
+    end
+
+    def request_handler
+      Kigo::Request.new(credentials, timeout: 40)
+    end
+
+    def credentials
+      Concierge::Credentials.for(supplier_name)
+    end
+
+    def supplier_name
+      Kigo::Client::SUPPLIER_NAME
+    end
+  end
+end
+
+# listen supplier worker
+Concierge::Announcer.on("availabilities.Kigo") do |host|
+  Workers::Suppliers::Kigo::Calendar.new(host).perform
+end
--- spec/workers/suppliers/kigo/legacy_spec.rb
+++ spec/workers/suppliers/kigo/legacy_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 
-RSpec.describe Workers::Suppliers::Kigo do
+RSpec.describe Workers::Suppliers::Kigo::Legacy do
   include Support::Fixtures
   include Support::Factories
 
-  let(:supplier) { create_supplier(name: 'Kigo') }
+  let(:supplier) { create_supplier(name: 'KigoLegacy') }
   let(:host) { create_host(supplier_id: supplier.id, identifier: '14908') }
   let(:properties_list) { JSON.parse(read_fixture('kigo/properties_list.json')) }
   let(:success_result) { Result.new(properties_list) }
@@ -17,8 +17,23 @@ RSpec.describe Workers::Suppliers::Kigo do
 
   subject { described_class.new(host) }
 
+  context 'deactivated host' do
+
+    it 'deletes host if his properties deactivated' do
+      allow_any_instance_of(Kigo::Importer).to receive(:fetch_properties) { Result.new(properties_list) }
+      allow_any_instance_of(Kigo::HostCheck).to receive(:deactivated?) { true }
+
+      expect(subject.synchronisation).not_to receive(:start)
+      subject.perform
+
+      expect(HostRepository.find(host.id)).to be_nil
+    end
+
+  end
+
   before do
     allow_any_instance_of(Kigo::Importer).to receive(:fetch_references) { references }
+    allow_any_instance_of(Kigo::HostCheck).to receive(:deactivated?) { false }
   end
 
   it 'announces an error if fetching properties fails' do
@@ -29,13 +44,14 @@ RSpec.describe Workers::Suppliers::Kigo do
     error = ExternalErrorRepository.last
 
     expect(error.operation).to eq 'sync'
-    expect(error.supplier).to eq 'Kigo'
+    expect(error.supplier).to eq 'KigoLegacy'
     expect(error.code).to eq 'json_rpc_response_has_errors'
   end
 
   it 'announces an error if fetching data fails' do
     allow_any_instance_of(Kigo::Importer).to receive(:fetch_properties) { Result.new(properties_list) }
     allow_any_instance_of(Kigo::Importer).to receive(:fetch_data) { Result.error(:connection_timeout) }
+    allow_any_instance_of(Kigo::HostCheck).to receive(:deactivated?) { false }
 
     subject.perform
 
@@ -43,7 +59,7 @@ RSpec.describe Workers::Suppliers::Kigo do
 
     expect(error.operation).to eq 'sync'
     expect(error.code).to eq 'connection_timeout'
-    expect(error.supplier).to eq 'Kigo'
+    expect(error.supplier).to eq 'KigoLegacy'
   end
 
   it 'does not process properties with unknown host' do
@@ -54,7 +70,7 @@ RSpec.describe Workers::Suppliers::Kigo do
   end
 
   context 'success' do
-    let(:property_data) { JSON.parse(read_fixture('kigo/property_data.json')) }
+    let(:property_data) { JSON.parse(read_fixture('kigo_legacy/property_data.json')) }
     let(:periodical_rates) { JSON.parse(read_fixture('kigo/pricing_setup.json')) }
 
     before do
