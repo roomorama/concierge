3a199d441e2e6c5824f0225b2edb864f03be3460
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-08-30 00:27:16 +0600

    Start to implemet property mapper

--- lib/concierge/suppliers/avantio/entities/accommodation.rb
+++ lib/concierge/suppliers/avantio/entities/accommodation.rb
@@ -1,29 +1,67 @@
 module Avantio
   module Entities
     class Accommodation
+
+      PROPERTY_ID_SEPARATOR = '|'
+
+      ATTRIBUTES = [
+        :accommodation_code, :user_code, :login_ga, :accommodation_name, :master_kind_code,
+        :country_iso_code, :city, :lat, :lng, :currency, :postal_code, :people_capacity,
+        :minimum_occupation, :bedrooms, :double_beds, :individual_beds, :individual_sofa_beds,
+        :double_sofa_beds, :berths, :housing_area, :area_unit, :pool_type, :tv, :garden,
+        :bbq, :terrace, :fenced_plot, :elevator, :dvd, :balcony, :gym,
+        :handicapped_facilities, :number_of_kitchens
+      ]
+
+      attr_reader *ATTRIBUTES
+
       def initialize(attrs)
-        @accommodation_code =   attrs[:accommodation_code]
-        @user_code =            attrs[:user_code]
-        @login_ga =             attrs[:login_ga]
-        @accommodation_name =   attrs[:accommodation_name]
-        @master_kind_code =     attrs[:master_kind_code]
-        @country_iso_code =     attrs[:country_iso_code]
-        @city =                 attrs[:city]
-        @lat =                  attrs[:lat]
-        @lng =                  attrs[:lng]
-        @currency =             attrs[:currency]
-        @postal_code =          attrs[:postal_code]
-        @people_capacity =      attrs[:people_capacity]
-        @minimum_occupation =   attrs[:minimum_occupation]
-        @bedrooms =             attrs[:bedrooms]
-        @double_beds =          attrs[:double_beds]
-        @individual_beds =      attrs[:individual_beds]
-        @individual_sofa_beds = attrs[:individual_sofa_beds]
-        @double_sofa_beds =     attrs[:double_sofa_beds]
-        @berths =               attrs[:berths]
-        @housing_area =         attrs[:housing_area]
-        @area_unit =            attrs[:area_unit]
-        #   add other data
+        @accommodation_code     = attrs[:accommodation_code]
+        @user_code              = attrs[:user_code]
+        @login_ga               = attrs[:login_ga]
+        @accommodation_name     = attrs[:accommodation_name]
+        @master_kind_code       = attrs[:master_kind_code]
+        @country_iso_code       = attrs[:country_iso_code]
+        @city                   = attrs[:city]
+        @lat                    = attrs[:lat]
+        @lng                    = attrs[:lng]
+        @street                 = attrs[:street]
+        @number                 = attrs[:number]
+        @block                  = attrs[:block]
+        @door                   = attrs[:door]
+        @currency               = attrs[:currency]
+        @district               = attrs[:district]
+        @postal_code            = attrs[:postal_code]
+        @people_capacity        = attrs[:people_capacity]
+        @minimum_occupation     = attrs[:minimum_occupation]
+        @bedrooms               = attrs[:bedrooms]
+        @double_beds            = attrs[:double_beds]
+        @individual_beds        = attrs[:individual_beds]
+        @individual_sofa_beds   = attrs[:individual_sofa_beds]
+        @double_sofa_beds       = attrs[:double_sofa_beds]
+        @berths                 = attrs[:berths]
+        @housing_area           = attrs[:housing_area]
+        @area_unit              = attrs[:area_unit]
+        @pool_type              = attrs[:pool_type]
+        @tv                     = attrs[:tv]
+        @fire_place             = attrs[:fire_place]
+        @garden                 = attrs[:garden]
+        @bbq                    = attrs[:bbq]
+        @terrace                = attrs[:terrace]
+        @fenced_plot            = attrs[:fenced_plot]
+        @elevator               = attrs[:elevator]
+        @dvd                    = attrs[:dvd]
+        @balcony                = attrs[:balcony]
+        @gym                    = attrs[:gym]
+        @handicapped_facilities = attrs[:handicapped_facilities] # need to be parsed
+        @number_of_kitchens     = attrs[:number_of_kitchens]
+      end
+
+      # Roomorama property id for given accommodation
+      def property_id
+        Avantio::PropertyId.from_avantio_ids(
+          accommodation_code, user_code, login_ga
+        ).property_id
       end
     end
   end
---
+++ lib/concierge/suppliers/avantio/mappers/roomorama_property.rb
@@ -0,0 +1,140 @@
+module Avantio
+  module Mappers
+    # +Avantio::Mappers::RoomoramaProperty+
+    #
+    # This class is responsible for building a +Roomorama::Property+ object
+    # from data getting from Avantio.
+    class RoomoramaProperty
+      CANCELLATION_POLICY = 'super_elite'
+      # Until we hear from Avantio on how to determine this field, it will be.. unknown
+      SECURITY_DEPOSIT_TYPE = 'unknown'
+
+      PROPERTY_TYPES = Concierge::SafeAccessHash.new({
+        1  => {type: 'apartment'}, # Apartment
+        2  => {type: 'house', subtype: 'villa'}, # Villa
+        14 => {type: 'apartment', subtype: 'studio_bachelor'}, # Studio
+        20 => {type: 'house', subtype: 'cottage'}, # Chalet
+        9  => {type: 'house', subtype: 'cottage'}, # Cottage
+        21 => {type: 'house', subtype: 'bungalow'} # Bungalow
+      })
+
+      # Maps Avantio data to +Roomorama::Property+
+      # Arguments
+      #
+      #   * +accommodation+ [Avantio::Entities::Accommodation]
+      # Returns +Roomorama::Property+
+      def build(accommodation)
+        result = Roomorama::Property.new(accommodation.property_id)
+        result.instant_booking!
+
+        set_base_info!(result, accommodation)
+        set_description!(result, description)
+        set_images!(result, images)
+        set_rates_and_minimum_stay!(result, rates)
+        set_security_deposit_info!(result, property, security_deposit)
+
+        result
+      end
+
+      private
+
+      def set_base_info!(result, accommodation)
+        result.title = accommodation.accommodation_name
+        result.type = PROPERTY_TYPES.get("#{accommodation.master_kind_code}.type")
+        result.subtype = PROPERTY_TYPES.get("#{accommodation.master_kind_code}.subtype")
+        result.address = fetch_address(accommodation)
+        result.postal_code = accommodation.postal_code
+        result.city = fetch_city(accommodation)
+        result.number_of_bedrooms = accommodation.bedrooms
+        result.max_guests = accommodation.people_capacity
+        result.apartment_number = accommodation.door
+
+
+
+        result.default_to_available = false
+        result.lat = property.xco
+        result.lng = property.yco
+        result.number_of_bathrooms = property.bathrooms
+        result.number_of_double_beds = calc_double_beds(property)
+        result.number_of_single_beds = calc_single_beds(property)
+        result.number_of_sofa_beds = calc_sofa_beds(property)
+        result.amenities = property.amenities
+        result.pets_allowed = property.pets_allowed
+        result.currency = property.currency_code
+        result.cancellation_policy = CANCELLATION_POLICY
+
+        country_code = country_converter.code_by_name(property.country)
+        result.country_code = country_code.value if country_code.success?
+      end
+
+      def fetch_address(accommodation)
+        [
+          accommodation.street,
+          accommodation.number,
+          accommodation.block
+        ].select { |x| !x.to_s.empty? }.join(', ') unless accommodation.street.to_s.empty?
+      end
+
+      # some city names contain multiple versions of the city name separated by
+      # a slash ("Gerona / Girona"). This is confusing geolocalization of the property
+      # on Roomorama. We just pick the first option in case this happens.
+      def fetch_city(accommodation)
+        accommodation.city&.split('/')&.first&.strip
+      end
+
+      def set_description!(result, description)
+        result.description = description
+      end
+
+      def calc_double_beds(property)
+        property.king_beds + property.queen_beds + property.full_beds
+      end
+
+      def calc_single_beds(property)
+        property.twin_beds + (property.extra_bed ? 1 : 0)
+      end
+
+      def calc_sofa_beds(property)
+        property.sofa_bed ? 1 : 0
+      end
+
+      def set_images!(result, images)
+        images.each do |url|
+          identifier = Digest::MD5.hexdigest(url)
+          image = Roomorama::Image.new(identifier)
+          image.url = url
+
+          result.add_image(image)
+        end
+      end
+
+      def set_rates_and_minimum_stay!(result, rates)
+        min_price = rates.map(&:daily_rate).min
+
+        result.minimum_stay = rates.map(&:min_nights_stay).min
+        result.nightly_rate = min_price
+        result.weekly_rate  = (min_price * 7).round(2)
+        result.monthly_rate = (min_price * 30).round(2)
+      end
+
+      def set_security_deposit_info!(result, property, security_deposit)
+        amount = extract_security_deposit_amount(security_deposit)
+        if amount
+          result.security_deposit_currency_code = property.currency_code
+          result.security_deposit_amount = amount
+          result.security_deposit_type = SECURITY_DEPOSIT_TYPE
+        end
+      end
+
+      def extract_security_deposit_amount(security_deposit)
+        if security_deposit && security_deposit.mandatory && security_deposit.flat_fee && security_deposit.flat_fee_amount != 0
+          security_deposit.flat_fee_amount
+        end
+      end
+
+      def country_converter
+        @country_converter ||= Ciirus::CountryCodeConverter.new
+      end
+    end
+  end
+end
