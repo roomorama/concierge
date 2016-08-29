6e901d243cec231f997113d5ff7ef2aea41d7d2c
Keang <skeang@gmail.com>
2016-08-29 13:03:00 +0800

    Rename

--- lib/concierge/suppliers/ciirus/validators/property_validator.rb
+++ lib/concierge/suppliers/ciirus/validators/property_validator.rb
@@ -9,7 +9,7 @@ module Ciirus
     #   * unknown country
     #
     class PropertyValidator
-      SUPPORTED_PROPERTY_TYPES = ['Office', 'Barn', 'Resort', 'Unspecified', 'Hotel']
+      UNSUPPORTED_PROPERTY_TYPES = ['Office', 'Barn', 'Resort', 'Unspecified', 'Hotel']
       attr_reader :property
 
       def initialize(property)
@@ -23,7 +23,7 @@ module Ciirus
       private
 
       def valid_property_type?
-        ! SUPPORTED_PROPERTY_TYPES.include?(property.type)
+        ! UNSUPPORTED_PROPERTY_TYPES.include?(property.type)
       end
     end
   end
