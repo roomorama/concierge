82f3c6834d2a3957f810a47aa5ffe10bc00bda3a
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-08-30 07:11:36 +0600

    Woori: correct date format

--- spec/lib/concierge/suppliers/woori/mappers/reservation_spec.rb
+++ spec/lib/concierge/suppliers/woori/mappers/reservation_spec.rb
@@ -15,8 +15,8 @@ module Woori
       API::Controllers::Params::MultiUnitBooking.new(
         property_id: '1',
         unit_id: '9733',
-        check_in: '02/02/2016',
-        check_out: '03/02/2016',
+        check_in: '2016-02-02',
+        check_out: '2016-02-03',
         guests: 1,
         currency_code: 'EUR',
         subtotal: '123.45',
@@ -37,8 +37,8 @@ module Woori
       expect(reservation).to be_kind_of(::Reservation)
       expect(reservation.property_id).to eq("1")
       expect(reservation.unit_id).to eq("9733")
-      expect(reservation.check_in).to eq("02/02/2016")
-      expect(reservation.check_out).to eq("03/02/2016")
+      expect(reservation.check_in).to eq("2016-02-02")
+      expect(reservation.check_out).to eq("2016-02-03")
       expect(reservation.guests).to eq(1)
       expect(reservation.reference_number).to eq("w_WP20160729141224FE3E")
       expect(reservation.customer).to eq(
