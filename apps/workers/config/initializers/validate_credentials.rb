e22beff0badf5ca112635817af78ca5cfbf584ba
Renato Mascarenhas <renato@roomorama.com>
2016-08-30 15:34:51 +0800

    Fix syntax error.

--- apps/workers/config/initializers/validate_credentials.rb
+++ apps/workers/config/initializers/validate_credentials.rb
@@ -2,7 +2,7 @@ enforce_on_envs = ["staging", "production"]
 
 if enforce_on_envs.include?(Hanami.env)
   Concierge::Credentials.validate_credentials!({
-    aws:   %w(region access_key_id secret_access_key sqs_queue_name s3_bucket)
+    aws:   %w(region access_key_id secret_access_key sqs_queue_name s3_bucket),
     woori: %w(url api_key properties_import_file units_1_import_file units_2_import_file units_3_import_file)
   })
 end
