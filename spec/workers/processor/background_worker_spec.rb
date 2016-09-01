d60276345e8188f1ad6738f888b9ec3bf3eab7d0
Renato Mascarenhas <renato@roomorama.com>
2016-09-01 14:16:52 +0800

    Actually skips a background worker if the ID is not recognised.

--- spec/workers/processor/background_worker_spec.rb
+++ spec/workers/processor/background_worker_spec.rb
@@ -154,8 +154,8 @@ RSpec.describe Workers::Processor::BackgroundWorker do
 
       result = subject.run
       expect(result).to be_a Result
-      expect(result).not_to be_success
-      expect(result.error.code).to eq :invalid_worker_id
+      expect(result).to be_success
+      expect(result.value).to eq :invalid_worker_id
 
       expect(error).to be_a Workers::Processor::BackgroundWorker::UnknownWorkerError
     end
