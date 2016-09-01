aa391659f16d2085d8c58d1269f2a6907d612d5f
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-09-02 00:49:15 +0600

    Add specs for services mapper

--- spec/lib/concierge/suppliers/avantio/mappers/services_spec.rb
+++ spec/lib/concierge/suppliers/avantio/mappers/services_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 RSpec.describe Avantio::Mappers::Services do
   include Support::Fixtures
 
-  let(:accommodation_with_deposit) { xml_from_file('avantio/accommodation_with_deposit.xml').at_xpath('AccommodationData') }
+  let(:accommodation_with_all_services) { xml_from_file('avantio/accommodation_with_all_services.xml').at_xpath('AccommodationData') }
+  let(:accommodation_with_not_all_year_services) { xml_from_file('avantio/accommodation_with_not_all_year_services.xml').at_xpath('AccommodationData') }
+  let(:accommodation_without_services) { xml_from_file('avantio/accommodation_without_services.xml').at_xpath('AccommodationData') }
 
   it 'returns mapped services' do
-    services = subject.build(accommodation_with_deposit)
+    services = subject.build(accommodation_with_all_services)
 
     expect(services[:security_deposit_amount]).to eq(1000.0)
     expect(services[:security_deposit_type]).to eq('cash')
@@ -28,9 +30,17 @@ RSpec.describe Avantio::Mappers::Services do
     expect(services[:airconditioning]).to be true
   end
 
-  # TODO empty xml
-  # TODO not all the season
-  # TODO bad structure
+  it 'returns empty services for empty xml' do
+    services = subject.build(accommodation_without_services)
+
+    expect(services.length).to eq(0)
+  end
+
+  it 'does not return services without full season' do
+    services = subject.build(accommodation_with_not_all_year_services)
+
+    expect(services.length).to eq(0)
+  end
 
   def xml_from_file(filename)
     Nokogiri::XML(read_fixture(filename))
