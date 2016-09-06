cba13022c12ea42c76086a2b05b630aae43b6e4a
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-09-06 22:06:51 +0600

    Spec for accommodations fetcher

--- lib/concierge/suppliers/avantio/commands/accommodations_fetcher.rb
+++ lib/concierge/suppliers/avantio/commands/accommodations_fetcher.rb
@@ -2,7 +2,20 @@ module Avantio
   module Commands
     # +Avantio::Commands::AccommodationsFetcher+
     #
+    # Avantio provides accommodations' information in file available by URL.
+    # This class fetches the file and parses the accommodations to the
+    # array of +Avantio::Entities::Accommodation+
     #
+    # Usage
+    #
+    #   command = Avantio::Commands::AccommodationsFetcher.new(code_partner)
+    #   result = command.call
+    #
+    #   if result.success?
+    #     result.value # Array of Accommodation instance
+    #   end
+    # The +call+ method returns a +Result+ object that, when successful,
+    # encapsulates the array of +Avantio::Entities::Accommodation+.
     class AccommodationsFetcher
       CODE = 'accommodations'
 
