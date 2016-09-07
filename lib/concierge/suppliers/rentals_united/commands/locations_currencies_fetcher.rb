5504f43cf6d4c9d958766aa1cc984f6bac8f3ea9
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-09-07 08:04:45 +0600

    RU: split method into two

--- lib/concierge/suppliers/rentals_united/commands/locations_currencies_fetcher.rb
+++ lib/concierge/suppliers/rentals_united/commands/locations_currencies_fetcher.rb
@@ -16,23 +16,29 @@ module RentalsUnited
         result_hash = response_parser.to_hash(result.value.body)
 
         if valid_status?(result_hash, ROOT_TAG)
-          location_currencies = {}
-
-          currencies_hash = result_hash.get("#{ROOT_TAG}.Currencies.Currency")
-          currencies_hash.each do |currency_hash|
-            safe_hash = Concierge::SafeAccessHash.new(currency_hash)
-            location_ids = Array(safe_hash.get("Locations.LocationID"))
-            currency_code = safe_hash.get("@CurrencyCode")
-
-            location_ids.each do |location_id|
-              location_currencies[location_id] = currency_code
-            end
-          end
-
+          location_currencies = build_location_currencies(result_hash)
           Result.new(location_currencies)
         else
           error_result(result_hash, ROOT_TAG)
         end
+      end
+
+      private
+      def build_location_currencies(result_hash)
+        location_currencies = {}
+
+        currencies_hash = result_hash.get("#{ROOT_TAG}.Currencies.Currency")
+        currencies_hash.each do |currency_hash|
+          safe_hash = Concierge::SafeAccessHash.new(currency_hash)
+          location_ids = Array(safe_hash.get("Locations.LocationID"))
+          currency_code = safe_hash.get("@CurrencyCode")
+
+          location_ids.each do |location_id|
+            location_currencies[location_id] = currency_code
+          end
+        end
+
+        location_currencies
       end
     end
   end
