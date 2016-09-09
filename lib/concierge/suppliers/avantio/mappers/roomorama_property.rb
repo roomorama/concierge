b524c81867e1d11b90f3e4c955a6432bd65d0a4b
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-09-09 11:23:40 +0600

    Add `initialize` to `mappers::RoomoramaProperty`

--- lib/concierge/suppliers/avantio/mappers/roomorama_property.rb
+++ lib/concierge/suppliers/avantio/mappers/roomorama_property.rb
@@ -19,7 +19,8 @@ module Avantio
         21 => {type: 'house', subtype: 'bungalow'} # Bungalow
       })
 
-      # Maps Avantio data to +Roomorama::Property+
+      attr_reader :accommodation, :description, :occupational_rule, :rate, :length
+
       # Arguments
       #
       #   * +accommodation+ [Avantio::Entities::Accommodation]
@@ -29,27 +30,36 @@ module Avantio
       #   * +length+ [Fixnum] all operations (calc of min_stay, calc of nightly_rate)
       #                       will be in daterange from today to today + length
       # Returns +Roomorama::Property+
-      def build(accommodation, description, occupational_rule, rate, length)
+      def initialize(accommodation, description, occupational_rule, rate, length)
+        @accommodation     = accommodation
+        @description       = description
+        @occupational_rule = occupational_rule
+        @rate              = rate
+        @length            = length
+      end
+
+      # Maps Avantio data to +Roomorama::Property+
+      def build
         result = Roomorama::Property.new(accommodation.property_id)
         result.instant_booking!
 
-        set_base_info!(result, accommodation)
-        set_amenities!(result, accommodation)
-        set_description!(result, description)
-        set_images!(result, description)
-        set_minimum_stay!(result, occupational_rule, length)
-        set_rates!(result, rate, length)
+        set_base_info!(result)
+        set_amenities!(result)
+        set_description!(result)
+        set_images!(result)
+        set_minimum_stay!(result)
+        set_rates!(result)
 
         result
       end
 
       private
 
-      def set_base_info!(result, accommodation)
+      def set_base_info!(result)
         result.title = accommodation.name
         result.type = PROPERTY_TYPES.get("#{accommodation.master_kind_code}.type")
         result.subtype = PROPERTY_TYPES.get("#{accommodation.master_kind_code}.subtype")
-        result.address = fetch_address(accommodation)
+        result.address = fetch_address
         result.postal_code = accommodation.postal_code
         result.city = fetch_city(accommodation)
         result.number_of_bedrooms = accommodation.bedrooms
@@ -60,13 +70,13 @@ module Avantio
         result.lat = accommodation.lat
         result.lng = accommodation.lng
         result.default_to_available = false
-        result.number_of_bathrooms = fetch_bathrooms(accommodation)
+        result.number_of_bathrooms = fetch_bathrooms
         result.floor = accommodation.floor
         result.number_of_double_beds = accommodation.double_beds
         result.number_of_single_beds = accommodation.individual_beds
-        result.number_of_sofa_beds = fetch_sofa_beds(accommodation)
+        result.number_of_sofa_beds = fetch_sofa_beds
         result.surface = accommodation.housing_area
-        result.surface_unit = fetch_surface_unit(accommodation)
+        result.surface_unit = fetch_surface_unit
         result.pets_allowed = accommodation.pets_allowed
         result.currency = accommodation.currency
         result.cancellation_policy = CANCELLATION_POLICY
@@ -80,27 +90,27 @@ module Avantio
         result.services_cleaning_required = accommodation.services_cleaning_required
       end
 
-      def fetch_surface_unit(accommodation)
+      def fetch_surface_unit
         if accommodation.housing_area
           accommodation.area_unit == 'm' ? 'metric' : 'imperial'
         end
       end
 
-      def fetch_sofa_beds(accommodation)
+      def fetch_sofa_beds
         [
           accommodation.individual_sofa_beds,
           accommodation.double_sofa_beds
         ].compact.inject(:+)
       end
 
-      def fetch_bathrooms(accommodation)
+      def fetch_bathrooms
         [
           accommodation.bathtub_bathrooms,
           accommodation.shower_bathrooms
         ].compact.inject(:+)
       end
 
-      def fetch_address(accommodation)
+      def fetch_address
         [
           accommodation.street,
           accommodation.number,
@@ -115,7 +125,7 @@ module Avantio
         accommodation.city&.split('/')&.first&.strip
       end
 
-      def set_amenities!(result, accommodation)
+      def set_amenities!(result)
         amenities = []
         amenities << 'bed_linen_and_towels' if accommodation.bed_linen && accommodation.towels
         amenities << 'kitchen' if accommodation.number_of_kitchens.to_i > 0
@@ -125,27 +135,27 @@ module Avantio
         amenities << 'airconditioning' if accommodation.airconditioning
         amenities << 'laundry' if accommodation.washing_machine
         amenities << 'free_cleaning' if accommodation.free_cleaning
-        amenities << 'wheelchairaccess' if suitable_for_disabled?(accommodation)
-        amenities << 'pool' if pool?(accommodation)
+        amenities << 'wheelchairaccess' if suitable_for_disabled?
+        amenities << 'pool' if pool?
         amenities << 'gym' if accommodation.gym
         amenities << 'elevator' if accommodation.elevator
         amenities << 'balcony' if accommodation.balcony
-        amenities << 'outdoor_space' if outdoor_space?(accommodation)
+        amenities << 'outdoor_space' if outdoor_space?
 
         result.amenities = amenities
       end
 
-      def pool?(accommodation)
+      def pool?
         !['', 'ninguna'].include?(accommodation.pool_type)
       end
 
-      def suitable_for_disabled?(accommodation)
+      def suitable_for_disabled?
         # suitable for disabled, without-stairs
         ['apta-discapacitados',
          'sin-escaleras'].include?(accommodation.handicapped_facilities)
       end
 
-      def outdoor_space?(accommodation)
+      def outdoor_space?
         accommodation.fire_place ||
           accommodation.garden ||
           accommodation.bbq ||
@@ -153,12 +163,12 @@ module Avantio
           accommodation.fenced_plot
       end
 
-      def set_description!(result, description)
+      def set_description!(result)
         sanitized = Sanitize.fragment(description.description, elements: ['br'])
         result.description = sanitized
       end
 
-      def set_images!(result, description)
+      def set_images!(result)
         description.images.each do |url|
           identifier = Digest::MD5.hexdigest(url)
           image = Roomorama::Image.new(identifier)
@@ -168,11 +178,11 @@ module Avantio
         end
       end
 
-      def set_minimum_stay!(result, occupational_rule, length)
+      def set_minimum_stay!(result)
         result.minimum_stay = occupational_rule.min_nights(length)
       end
 
-      def set_rates!(result, rate, length)
+      def set_rates!(result)
         price = rate.min_price(length)
         if price
           result.nightly_rate = price
