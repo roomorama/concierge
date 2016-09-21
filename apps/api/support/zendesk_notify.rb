71d6b62361b2dfca5b887b7f236efc52fd23f110
Renato Mascarenhas <renato@roomorama.com>
2016-09-21 11:19:42 +0800

    Include ZendeskNotify client.

---
+++ apps/api/support/zendesk_notify.rb
@@ -0,0 +1,86 @@
+module API::Support
+
+  # +API::Support::ZendeskNotify+
+  #
+  # This is a client class to the +ZendeskNotify+ service, a simple API to
+  # send tickets on Roomorama/BridgeRentals Zendesk account.
+  #
+  # Usage
+  #
+  #   client = API::Support::ZendeskNotify.new
+  #   ticket_id = "cancellation"
+  #   attributes = {
+  #     supplier:    "Supplier X",
+  #     supplier_id: "212",
+  #     bridge_id:   "291"
+  #   }
+  #
+  #   client.notify # => <#Result value=true>
+  #
+  # The +attributes+ passed when calling the +notify+ method are dependent on the
+  # type of ticket being sent.
+  class ZendeskNotify
+    include Concierge::JSON
+
+    SUPPORTED_TICKETS = %w(cancellation)
+
+    attr_reader :http, :endpoint
+
+    # initializes internal state. The +ZendeskNotify+ service URL must be properly
+    # configured on the +ZENDESK_NOTIFY_URL+ environment variable.
+    def initialize
+      uri  = URI.parse(zendesk_notify_url)
+      host = [uri.scheme, "://", uri.host].join
+
+      @http     = Concierge::HTTPClient.new(host)
+      @endpoint = uri.request_uri
+    end
+
+    def notify(ticket_id, attributes)
+      return invalid_ticket unless SUPPORTED_TICKETS.include?(ticket_id.to_s)
+
+      params = {
+        ticketId:   ticket_id,
+        attributes: attributes
+      }
+
+      result = http.post(endpoint, params)
+      return result unless result.success?
+
+      parse_response(result.value.body)
+    end
+
+    private
+
+    # expected response (examples)
+    #
+    # Success
+    #   { "status": "ok", "message": "Ticket sent successfully" }
+    #
+    # Failure
+    #   { "status": "error", "message": "Failure to deliver ticket" }
+    def parse_response(body)
+      decoded_body = json_decode(body)
+      return decoded_body unless decoded_body.success?
+
+      successful = (decoded_body.value["status"] == "ok")
+
+      if successful
+        Result.new(true)
+      else
+        error_message = decoded_body.value["message"]
+        Result.error(:zendesk_notify_failure, error_message)
+      end
+    end
+
+    def invalid_ticket
+      Result.error(:zendesk_invalid_ticket)
+    end
+
+    def zendesk_notify_url
+      ENV["ZENDESK_NOTIFY_URL"]
+    end
+
+  end
+
+end
