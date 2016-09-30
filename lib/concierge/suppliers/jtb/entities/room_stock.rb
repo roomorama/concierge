9dc2fa77c0f637e846f892da0b73b2eca04f5a66
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-09-30 10:35:04 +0600

    Rename jtb_room_stock.option_plan_id column to rate_plan_id

--- lib/concierge/suppliers/jtb/entities/room_stock.rb
+++ lib/concierge/suppliers/jtb/entities/room_stock.rb
@@ -3,7 +3,7 @@ module JTB
     class RoomStock
       include Hanami::Entity
 
-      attributes :city_code, :hotel_code, :option_plan_id, :service_date, :number_of_units,
+      attributes :city_code, :hotel_code, :rate_plan_id, :service_date, :number_of_units,
                  :closing_date, :sale_status, :reservation_closing_date
     end
   end
