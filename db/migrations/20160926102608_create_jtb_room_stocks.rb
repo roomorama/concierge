9798e311399452cb818ad166f6b2be0e1dc5b3a8
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-09-30 10:35:04 +0600

    Rename jtb_room_stock.option_plan_id column to rate_plan_id

--- db/migrations/20160926102608_create_jtb_room_stocks.rb
+++ db/migrations/20160926102608_create_jtb_room_stocks.rb
@@ -4,7 +4,7 @@ Hanami::Model.migration do
       primary_key [:city_code, :hotel_code, :option_plan_id, :service_date]
       column :city_code,                String, null: false, size: 3
       column :hotel_code,               String, null: false, size: 3
-      column :option_plan_id,           String, null: false, size: 16
+      column :rate_plan_id,             String, null: false, size: 16
       column :service_date,             String, null: false, size: 8
       column :number_of_units,          Integer, null: false
       column :closing_date,             String, null: false, size: 8
