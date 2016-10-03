7c95a60c9ab6a2a0cc62873ba2151c0940d1bc9b
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-03 09:35:55 +0600

    Remove useless column from rate plans table

--- lib/concierge/suppliers/jtb/entities/rate_plan.rb
+++ lib/concierge/suppliers/jtb/entities/rate_plan.rb
@@ -3,7 +3,7 @@ module JTB
     class RatePlan
       include Hanami::Entity
 
-      attributes :language, :city_code, :hotel_code, :rate_plan_id, :room_code, :meal_plan_code, :occupancy
+      attributes :city_code, :hotel_code, :rate_plan_id, :room_code, :meal_plan_code, :occupancy
     end
   end
 end
