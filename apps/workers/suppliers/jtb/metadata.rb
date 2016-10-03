29ece446ba003267fd6bbfbdae24b9b29295d8a2
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-03 15:23:55 +0600

    Fix supplier name

--- apps/workers/suppliers/jtb/metadata.rb
+++ apps/workers/suppliers/jtb/metadata.rb
@@ -201,7 +201,7 @@ module Workers::Suppliers::JTB
 
       Concierge::Announcer.trigger(Concierge::Errors::EXTERNAL_ERROR, {
         operation:   'sync',
-        supplier:    Ciirus::Client::SUPPLIER_NAME,
+        supplier:    JTB::Client::SUPPLIER_NAME,
         code:        result.error.code,
         context:     Concierge.context.to_h,
         happened_at: Time.now
