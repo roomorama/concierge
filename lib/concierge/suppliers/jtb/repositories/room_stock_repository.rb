fe3537f6bd689da4c9322a64fea7f6f406480a0a
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-11 11:01:26 +0600

    Add room stocks actualizer

--- lib/concierge/suppliers/jtb/repositories/room_stock_repository.rb
+++ lib/concierge/suppliers/jtb/repositories/room_stock_repository.rb
@@ -29,6 +29,64 @@ module JTB
             .and("service_date between '#{from}' and '#{to}'")
         end
       end
+
+      def self.by_primary_key(city_code, hotel_code, rate_plan_id, service_date)
+        query do
+          where(city_code: city_code)
+            .and(hotel_code: hotel_code)
+            .and(rate_plan_id: rate_plan_id)
+            .and(service_date: service_date)
+        end.first
+      end
+
+      def self.upsert(attributes)
+        HotelRepository.adapter.instance_variable_get("@connection")[
+          'insert into jtb_room_stocks
+           (
+             city_code,
+             hotel_code,
+             rate_plan_id,
+             service_date,
+             number_of_units,
+             closing_date,
+             sale_status,
+             reservation_closing_date
+           ) values (
+             :city_code,
+             :hotel_code,
+             :rate_plan_id,
+             :service_date,
+             :number_of_units,
+             :closing_date,
+             :sale_status,
+             :reservation_closing_date
+           )
+           on conflict (
+             city_code,
+             hotel_code,
+             rate_plan_id,
+             service_date
+           ) do update set
+             number_of_units = :number_of_units,
+             closing_date = :closing_date,
+             sale_status = :sale_status,
+             reservation_closing_date = :reservation_closing_date
+          ',
+          attributes
+        ].first
+      end
+
+      def self.delete(attributes)
+        RoomStockRepository.adapter.instance_variable_get("@connection")[
+          'delete from jtb_room_stocks
+           where city_code = :city_code
+             and hotel_code = :hotel_code
+             and rate_plan_id = :rate_plan_id
+             and service_date = :service_date
+          ',
+          attributes
+        ].first
+      end
     end
   end
 end
