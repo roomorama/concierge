cb4f94583b2faa747c026d1ed814248c4975c946
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-11 12:29:34 +0600

    Add room type actualizer

---
+++ lib/concierge/suppliers/jtb/sync/db/room_types_actualizer.rb
@@ -0,0 +1,114 @@
+module JTB
+  module Sync
+    module DB
+      # +JTB::Sync::RoomTypesActualizer+
+      #
+      # Class responsible for actualizing DB jtb_room_types table
+      class RoomTypesActualizer < BaseActualizer
+
+        FILE_PREFIX = 'RoomType'
+
+        # mapping between CSV columns and DB table columns,
+        MAPPING = {
+          0  => :language,
+          1  => :city_code,
+          2  => :hotel_code,
+          3  => :room_code,
+          4  => :room_grade,
+          5  => :room_type_code,
+          6  => :room_name,
+          11 => :min_guests,
+          12 => :max_guests,
+          13 => :extra_bed,
+          14 => :extra_bed_type,
+          15 => :size1,
+          16 => :size2,
+          17 => :size3,
+          18 => :size4,
+          19 => :size5,
+          20 => :size6,
+        }
+
+        UPDATE_CATEGORY_INDEX = 123
+
+        protected
+
+        def import_file(file_path)
+          File.open(file_path) do |file|
+            indexes = MAPPING.keys
+            repository.copy_csv_into do
+              line = file.gets
+              fetch_required_columns(line, indexes) if line
+            end
+          end
+        end
+
+        def import_diff_file(file_path)
+          File.open(file_path) do |file|
+            # Use savepoint here to be sure of new transaction.
+            # Because by default Sequel will reuse an existing transaction
+            repository.transaction(rollback: :reraise, savepoint: true) do
+              file.each_line do |line|
+                attributes = build_attributes(line, MAPPING)
+                update_category = extract_update_category(line)
+                if update_category <= 2
+                  repository.upsert(attributes)
+                else
+                  repository.delete(attributes)
+                end
+              end
+            end
+          end
+        end
+
+        def file_prefix
+          FILE_PREFIX
+        end
+
+        def repository
+          JTB::Repositories::RoomTypeRepository
+        end
+
+        private
+
+        def extract_update_category(line)
+          line.split(CSV_DELIMITER, -1)[UPDATE_CATEGORY_INDEX].to_i
+        end
+
+        def build_attributes(line, columns_mapping)
+          # Remove last \n from string
+          line = line[0..-2]
+
+          # postgresql copy into writes empty strings as NULL.
+          # To make Diff import the same replace all empty strings with nil
+          splitted = line.split(CSV_DELIMITER, -1).map { |e| e.empty? ? nil : e }
+
+          attributes = columns_mapping.map do |k, v|
+            [v, splitted[k]]
+          end.to_h
+          attributes[:amenities] = fetch_amenities(line)
+
+          attributes
+        end
+
+        def fetch_required_columns(line, indexes)
+          # Remove last \n from string
+          line = line[0..-2]
+          values = line.split(CSV_DELIMITER, -1)
+          required_columns = values.values_at(*indexes).map { |e| e.empty? ? nil : e }
+          compressed_amenities = fetch_amenities(line)
+          required_columns << compressed_amenities
+
+          required_columns.join(CSV_DELIMITER) + "\n"
+        end
+
+        def fetch_amenities(line)
+          values = line.split(CSV_DELIMITER, -1)
+          # File contains 100 amenities columns with values '1', '0' or ''
+          # let's join all amenities values in one
+          values[23..122].map { |a| a.to_s.empty? ? ' ' : a.to_s }.join
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/concierge/suppliers/jtb/sync/db/room_types_actualizer_spec.rb
@@ -0,0 +1,160 @@
+require 'spec_helper'
+
+RSpec.describe JTB::Sync::DB::RoomTypesActualizer do
+  let(:room_type_attributes) do
+    {
+      language: "EN",
+      city_code: "CHU",
+      hotel_code: "W01",
+      room_code: "CHUHW01RM0000001",
+      room_grade: "STD",
+      room_type_code: "SGL",
+      room_name: "Single A",
+      min_guests: "1",
+      max_guests: "1",
+      extra_bed: nil,
+      extra_bed_type: nil,
+      size1: "15.10",
+      size2: nil,
+      size3: nil,
+      size4: nil,
+      size5: nil,
+      size6: nil,
+      amenities: "100 0000010111011011101000000001000000010010000001000000101110010000001100000010111 11 01000010000  "
+   }
+  end
+
+  subject { described_class.new(tmp_path)}
+
+  describe '#actualize' do
+    context 'when diff file is empty' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'empty_diff') }
+
+      it 'does nothing' do
+        create_room_type(room_type_attributes)
+
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        room_types = JTB::Repositories::RoomTypeRepository.all
+        expect(room_types.length).to eq(1)
+      end
+    end
+
+    context 'when diff file contains create update_category' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'create') }
+
+      it 'creates new room type' do
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('EN', 'CHU', 'W01', 'CHUHW01RM0000001')
+        expect(room_type).to be_nil
+
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        room_types = JTB::Repositories::RoomTypeRepository.all
+        expect(room_types.length).to eq(1)
+
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('EN', 'CHU', 'W01', 'CHUHW01RM0000001')
+        expect(room_type).not_to be_nil
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_Diff_20161010013224.zip')
+      end
+    end
+
+    context 'when diff file contains update update_category' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'update') }
+
+      it 'updates room type' do
+        create_room_type(room_type_attributes)
+        create_room_type(room_type_attributes.merge({hotel_code: 'W02' }))
+
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        # Update room type
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('EN', 'CHU', 'W01', 'CHUHW01RM0000001')
+        expect(room_type.amenities).to eq(room_type_attributes[:amenities][0..-2] + '1')
+
+        # Does not update another room type
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('EN', 'CHU', 'W02', 'CHUHW01RM0000001')
+        expect(room_type.amenities).to eq(room_type_attributes[:amenities])
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_Diff_20161010013225.zip')
+      end
+    end
+
+    context 'when directory contains ALL file' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'with_all') }
+
+      it 'imports the room types from the file' do
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        room_types = JTB::Repositories::RoomTypeRepository.all
+        expect(room_types.length).to eq(13)
+
+        room_type = room_types.find { |r| r.room_code == 'CHUHW01RM0000001' }
+        expect(room_type.amenities).to eq(room_type_attributes[:amenities])
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_ALL_20161010.zip')
+      end
+
+      it 'clear table before actualisation' do
+        create_room_type(room_type_attributes.merge({language: 'QQ', city_code: 'QQQ', hotel_code: 'QQQ' }))
+
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('QQ', 'QQQ', 'QQQ', 'CHUHW01RM0000001')
+        expect(room_type).to be_nil
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_ALL_20161010.zip')
+      end
+    end
+
+    context 'when diff file contains delete update_category' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'delete') }
+
+      it 'delete the room type' do
+        create_room_type(room_type_attributes)
+
+        result = subject.actualize
+        expect(result.success?).to be true
+
+        room_types = JTB::Repositories::RoomTypeRepository.all
+        expect(room_types.length).to eq(0)
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_Diff_20161010013226.zip')
+      end
+    end
+
+    context 'when there is some problem during some file actualization' do
+      let(:tmp_path) { Hanami.root.join('spec', 'fixtures', 'jtb', 'sync', 'room_types', 'transaction') }
+
+      it 'imports only files before invalid' do
+        result = subject.actualize
+        expect(result.success?).to be false
+
+        room_types = JTB::Repositories::RoomTypeRepository.all
+        expect(room_types.length).to eq(1)
+
+        room_type = JTB::Repositories::RoomTypeRepository.by_primary_key('EN', 'CHU', 'W01', 'CHUHW01RM0000001')
+        expect(room_type).not_to be_nil
+
+        state = JTB::Repositories::StateRepository.by_prefix('RoomType')
+        expect(state.file_name).to eq('RoomType_Diff_20161010013227.zip')
+      end
+    end
+
+    def create_room_type(attributes)
+      JTB::Repositories::RoomTypeRepository.create(
+        JTB::Entities::RoomType.new(attributes)
+      )
+    end
+  end
+end
