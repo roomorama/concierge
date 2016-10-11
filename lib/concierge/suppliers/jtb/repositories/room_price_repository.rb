b9d8d340fd14d6e9055a1793b927e33efe94b5b7
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-11 09:32:24 +0600

    Add room price actualizer

--- lib/concierge/suppliers/jtb/repositories/room_price_repository.rb
+++ lib/concierge/suppliers/jtb/repositories/room_price_repository.rb
@@ -22,6 +22,55 @@ module JTB
             .and(date: date)
         end.min(:room_rate)
       end
+
+      def self.by_primary_key(city_code, hotel_code, rate_plan_id, date)
+        query do
+          where(city_code: city_code)
+            .and(hotel_code: hotel_code)
+            .and(rate_plan_id: rate_plan_id)
+            .and(date: date)
+        end.first
+      end
+
+      def self.upsert(attributes)
+        RoomPriceRepository.adapter.instance_variable_get("@connection")[
+          'insert into jtb_room_prices
+           (
+             city_code,
+             hotel_code,
+             rate_plan_id,
+             date,
+             room_rate
+           ) values (
+             :city_code,
+             :hotel_code,
+             :rate_plan_id,
+             :date,
+             :room_rate
+           )
+           on conflict (
+             city_code,
+             hotel_code,
+             rate_plan_id,
+             date
+           ) do update set
+             room_rate = :room_rate
+          ',
+          attributes
+        ].first
+      end
+
+      def self.delete(attributes)
+        RoomPriceRepository.adapter.instance_variable_get("@connection")[
+          'delete from jtb_room_prices
+           where city_code = :city_code
+             and hotel_code = :hotel_code
+             and rate_plan_id = :rate_plan_id
+             and date = :date
+          ',
+          attributes
+        ].first
+      end
     end
   end
 end
