772e5a08184f3fcd4f9343ce96d1b596915f4dd3
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-24 00:15:03 +0200

    Add cancel controller

--- apps/api/config/routes.rb
+++ apps/api/config/routes.rb
@@ -26,6 +26,7 @@ post '/kigo/legacy/cancel',   to: 'kigo/legacy#cancel'
 post '/poplidays/cancel',     to: 'poplidays#cancel'
 post '/atleisure/cancel',     to: 'at_leisure#cancel'
 post '/rentalsunited/cancel', to: 'rentals_united#cancel'
+post '/jtb/cancel',           to: 'j_t_b#cancel'
 
 post '/checkout', to: 'static#checkout'
 get '/kigo/image/:property_id/:image_id', to: 'kigo#image'
