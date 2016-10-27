df7626e73cb167d3a58380eaa19bcf1aff2a240f
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-10-27 19:37:52 +0600

    Kigo: beginning if unifying error messages for response parser

--- lib/concierge/suppliers/kigo/response_parser.rb
+++ lib/concierge/suppliers/kigo/response_parser.rb
@@ -17,6 +17,7 @@ module Kigo
     include Concierge::JSON
 
     DATES_NOT_AVAILABLE_MSG = "Dates not available"
+    CHECK_IN_TOO_FAR_MSG = "Unfortunately we do not allow bookings over 366 days into the future"
 
     attr_reader :params
 
@@ -38,8 +39,7 @@ module Kigo
       decoded_payload = json_decode(response)
       return decoded_payload unless decoded_payload.success?
 
-      payload   = decoded_payload.value
-      quotation = build_quotation(params)
+      payload = decoded_payload.value
 
       if payload["API_RESULT_CODE"] == "E_OK"
         reply = payload["API_REPLY"]
@@ -51,43 +51,12 @@ module Kigo
         total = reply["TOTAL_AMOUNT"]
         return missing_field_error("TOTAL_AMOUNT") unless total
 
-        quotation.available           = true
-        quotation.currency            = currency
-        quotation.total               = total.to_f
-
+        quotation = build_available_quotation(params, currency, total)
         Result.new(quotation)
-      elsif payload["API_RESULT_CODE"] == "E_NOSUCH" && payload["API_RESULT_TEXT"] =~ /is not available for your selected period/
-        # Kigo uses the same result code (+E_NOSUCH+) to indicate when a property ID
-        # is unknown and when the property exists, but is unavailable for the selected
-        # dates. To determine which is the case, it is necessary to parse the
-        # +API_RESULT_TEXT+ and check if it mentions that the property is unavailable
-        # for the requested dates.
-
-        quotation.available = false
-        Result.new(quotation)
-      elsif payload["API_RESULT_CODE"] == "E_LIMIT"
-        # The only possible message for an +E_LIMIT+ error, according to Kigo's
-        # documentation, is for the following message:
-        #
-        #   The property pricing information is unavailable for the specified check-in/check-out dates
-        #
-        # When there are no rates available for the selected dates, this message is returned
-        # by Kigo and KigoLegacy's API. This is not an error situation, and the property
-        # should be deemed unavailable.
-
-        quotation.available = false
-        Result.new(quotation)
-      elsif payload["API_RESULT_CODE"] == "E_EMPTY"
-        # Most often, the error message associated with the +E_EMPTY+ errror is
-        #
-        #   The property pricing information is unavailable or the property pricing calculation is disabled.
-        #
-        # This means that either there are no availabilities for the property,
-        # or the selected dates are already booked. In either case, the property
-        # should be considered unavailable.
-
-        quotation.available = false
-        Result.new(quotation)
+      elsif error_should_be_silenced?(payload)
+        Result.new(build_unavailable_quotation(params))
+      elsif check_in_too_far?(payload)
+        check_in_too_far_error
       else
         non_successful_result_error(:quote_call_failed)
       end
@@ -144,12 +113,74 @@ module Kigo
 
     private
 
+    # Not all errors from Kigo API are errors in terms of our business logic,
+    # so this method determines whether we should consider error from Kigo API
+    # as normal response or not.
+    def error_should_be_silenced?(payload)
+      property_not_available_for_selected_period?(payload) ||
+      property_pricing_not_available_for_selected_period?(payload) ||
+      property_pricing_not_available_or_calculation_is_disabled?(payload)
+    end
+
+    # Kigo uses the same result code (+E_NOSUCH+) to indicate when a property ID
+    # is unknown and when the property exists, but is unavailable for the selected
+    # dates. To determine which is the case, it is necessary to parse the
+    # +API_RESULT_TEXT+ and check if it mentions that the property is unavailable
+    # for the requested dates.
+    def property_not_available_for_selected_period?(payload)
+      payload["API_RESULT_CODE"] == "E_NOSUCH" && payload["API_RESULT_TEXT"] =~ /is not available for your selected period/
+    end
+
+    # The only possible message for an +E_LIMIT+ error, according to Kigo's
+    # documentation, is for the following message:
+    #
+    #   The property pricing information is unavailable for the specified check-in/check-out dates
+    #
+    # When there are no rates available for the selected dates, this message is returned
+    # by Kigo and KigoLegacy's API. This is not an error situation, and the property
+    # should be deemed unavailable.
+    def property_pricing_not_available_for_selected_period?(payload)
+      payload["API_RESULT_CODE"] == "E_LIMIT"
+    end
+
+    # Most often, the error message associated with the +E_EMPTY+ errror is
+    #
+    #   The property pricing information is unavailable or the property pricing calculation is disabled.
+    #
+    # This means that either there are no availabilities for the property,
+    # or the selected dates are already booked. In either case, the property
+    # should be considered unavailable.
+    def property_pricing_not_available_or_calculation_is_disabled?(payload)
+      payload["API_RESULT_CODE"] == "E_EMPTY"
+    end
+
+    # Error message is fired when user tries to quote prices with check_in
+    # date in too far future.
+    #
+    # In that case Kigo's API original error message is:
+    #
+    # The property pricing information is unavailable due to a constraint.
+    # Unfortunately we do not allow bookings over 366 days into the future.
+    def check_in_too_far?(payload)
+      payload["API_RESULT_CODE"] == "E_CONFLICT" && payload["API_RESULT_TEXT"] =~ /#{CHECK_IN_TOO_FAR_MSG}/
+    end
+
     def build_reservation(params)
       Reservation.new(params)
     end
 
-    def build_quotation(params)
-      Quotation.new(params)
+    def build_available_quotation(params, currency, total)
+      quotation = Quotation.new(params)
+      quotation.available = true
+      quotation.currency  = currency
+      quotation.total     = total.to_f
+      quotation
+    end
+
+    def build_unavailable_quotation(params)
+      quotation = Quotation.new(params)
+      quotation.available = false
+      quotation
     end
 
     def non_successful_result_error(error_code)
@@ -158,6 +189,10 @@ module Kigo
 
       mismatch(message, caller)
       Result.error(error_code, message)
+    end
+
+    def check_in_too_far_error
+      Result.error(:check_in_too_far, "Selected check-in date is too far")
     end
 
     def dates_not_available_error
