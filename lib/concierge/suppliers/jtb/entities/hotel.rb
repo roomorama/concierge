2a6f34af893ffb294a6b59419728832d006c1335
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-10-31 00:03:30 +0100

    Add property amenities parsing

--- lib/concierge/suppliers/jtb/entities/hotel.rb
+++ lib/concierge/suppliers/jtb/entities/hotel.rb
@@ -9,6 +9,20 @@ module JTB
                  :indoor_pool_charged, :outdoor_pool_free, :outdoor_pool_charged, :indoor_gym_free,
                  :indoor_gym_charged, :outdoor_gym_free, :outdoor_gym_charged, :wheelchair_access,
                  :check_in, :check_out
+
+      def has_pool?
+        indoor_pool_free == '1' ||
+          indoor_pool_charged == '1' ||
+          outdoor_pool_free == '1' ||
+          outdoor_pool_charged == '1'
+      end
+
+      def has_gym?
+        indoor_gym_free == '1' ||
+          indoor_gym_charged == '1' ||
+          outdoor_gym_free == '1' ||
+          outdoor_gym_charged == '1'
+      end
     end
   end
 end
