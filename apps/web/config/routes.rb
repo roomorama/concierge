d2ee9c4d95552e79768b944771e6afaf18542d85
Sharipov Ruslan <sharipov.ru@gmail.com>
2016-10-31 13:52:56 +0600

    Add UI button to resync host

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,5 +1,6 @@
-get "/sync_processes/:id/stats", to: "sync_processes#stats", as: :stats
-get "/",                         to: "dashboard#index",      as: :root
+get "/sync_processes/:id/stats",            to: "sync_processes#stats", as: :stats
+post "/suppliers/:supplier_id/resync_host", to: "hosts#resync",         as: :resync_host
+get "/",                                    to: "dashboard#index",      as: :root
 
 resources :errors,         only: [:index, :show], controller: "external_errors"
 resources :reservations,   only: [:index]
