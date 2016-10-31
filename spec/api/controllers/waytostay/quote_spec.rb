bc5106d006bf6be24d3efb0cbbe043a4b17cee06
Keang <skeang@gmail.com>
2016-10-31 14:54:44 +0800

    Abstract controller specs

--- spec/api/controllers/waytostay/quote_spec.rb
+++ spec/api/controllers/waytostay/quote_spec.rb
@@ -1,6 +1,7 @@
 require "spec_helper"
 require "concierge/result"
 require_relative "../shared/quote_validations"
+require_relative "../shared/quote_call"
 require_relative "../shared/external_error_reporting"
 
 RSpec.describe API::Controllers::Waytostay::Quote do
@@ -33,94 +34,6 @@ RSpec.describe API::Controllers::Waytostay::Quote do
     end
   end
 
-  describe "#call" do
-    subject { described_class.new.call(params) }
-
-    it "returns a proper error message if client returns network error" do
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(Result.error(:network_error))
-
-      response = parse_response(subject)
-      expect(response.status).to eq 503
-      expect(response.body["status"]).to eq "error"
-      expect(response.body["errors"]["quote"]).to eq "Could not quote price with remote supplier"
-    end
-
-    it "returns proper error messages if client returns check_in_too_near" do
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(check_in_too_near)
-      response = parse_response(subject)
-      expect(response.status).to eq 200
-      expect(response.body["status"]).to eq "error"
-      expect(response.body["errors"]["quote"]).to eq "Selected check-in date is too near"
-    end
-
-    it "returns proper error messages if client returns check_in_too_near" do
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(check_in_too_far)
-      response = parse_response(subject)
-      expect(response.status).to eq 200
-      expect(response.body["status"]).to eq "error"
-      expect(response.body["errors"]["quote"]).to eq "Selected check-in date is too far"
-    end
-
-    it "returns proper error messages if client returns stay_too_short" do
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(stay_too_short(15))
-      response = parse_response(subject)
-      expect(response.status).to eq 200
-      expect(response.body["status"]).to eq "error"
-      expect(response.body["errors"]["quote"]).to eq "The minimum number of nights to book this apartment is 15"
-    end
-
-    it "returns unavailable quotation when client returns so" do
-      unavailable_quotation = Quotation.new({
-        property_id: params[:property_id],
-        check_in:    params[:check_in],
-        check_out:   params[:check_out],
-        guests:      params[:guests],
-        available:   false
-      })
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(Result.new(unavailable_quotation))
-
-      response = parse_response(subject)
-      expect(response.status).to eq 200
-      expect(response.body["status"]).to eq "ok"
-      expect(response.body["available"]).to eq false
-      expect(response.body["property_id"]).to eq "567"
-      expect(response.body["check_in"]).to eq "2016-03-22"
-      expect(response.body["check_out"]).to eq "2016-03-25"
-      expect(response.body["guests"]).to eq 2
-      expect(response.body).not_to have_key("currency")
-      expect(response.body).not_to have_key("total")
-    end
-
-    it "returns available quotation when call is successful" do
-      available_quotation = Quotation.new({
-          property_id:         params[:property_id],
-          check_in:            params[:check_in],
-          check_out:           params[:check_out],
-          guests:              params[:guests],
-          available:           true,
-          currency:            "EUR",
-          total:               56.78,
-          host_fee_percentage: 7.0,
-          host_fee:            3.71 ,
-          net_rate:            53.07,
-        })
-      expect_any_instance_of(Waytostay::Client).to receive(:quote).and_return(Result.new(available_quotation))
-
-      response = parse_response(subject)
-      expect(response.status).to eq 200
-      expect(response.body["status"]).to eq "ok"
-      expect(response.body["available"]).to eq true
-      expect(response.body["property_id"]).to eq "567"
-      expect(response.body["check_in"]).to eq "2016-03-22"
-      expect(response.body["check_out"]).to eq "2016-03-25"
-      expect(response.body["guests"]).to eq 2
-      expect(response.body["currency"]).to eq "EUR"
-      expect(response.body["total"]).to eq 56.78
-      expect(response.body["net_rate"]).to eq 53.07
-      expect(response.body["host_fee"]).to eq 3.71
-      expect(response.body["host_fee_percentage"]).to eq 7.0
-
-    end
-  end
+  it_behaves_like "quote call"
 
 end
