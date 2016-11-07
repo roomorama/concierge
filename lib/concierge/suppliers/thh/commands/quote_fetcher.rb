010c5ba2c86256519d4d9f8bcd9a27642934f508
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-11-07 13:31:46 +0300

    Implement quote fetcher

---
+++ lib/concierge/suppliers/thh/commands/quote_fetcher.rb
@@ -0,0 +1,66 @@
+module THH
+  module Commands
+    #  +THH::Commands::PropertyFetcher+
+    #
+    # This class is responsible for fetching availability information
+    # (including price) from THH API and parsing the response
+    # to +Concierge::SafeAccessHash+.
+    #
+    # Usage
+    #
+    #   result = THH::Commands::QuoteFetcher.new(credentials).call(params)
+    #   if result.success?
+    #     result.value # SafeAccessHash with availability info
+    #   end
+    #
+    # The +call+ method returns a +Result+ object that, when successful,
+    # encapsulates SafeAccessHash.
+    class QuoteFetcher < BaseFetcher
+      ROOMORAMA_DATE_FORMAT = '%Y-%m-%d'
+      THH_DATE_FORMAT = '%d/%m/%Y'
+      REQUIRED_FIELDS = ['response.available', 'response.price']
+
+      def call(params)
+        result = api_call(params(params))
+        return result unless result.success?
+
+        response = Concierge::SafeAccessHash.new(result.value)
+        result = validate_response(response, params)
+        return result unless result.success?
+
+        Result.new(response)
+      end
+
+      protected
+
+      def action
+        'availability'
+      end
+
+      private
+
+      def validate_response(response, params)
+        REQUIRED_FIELDS.each do |field|
+          unless response.get(field)
+            return Result.error(:unrecognised_response, "Available response for params `#{params.to_h}` does not contain `#{field}` field")
+          end
+        end
+        Result.new(true)
+      end
+
+      def params(params)
+        {
+          'arrival'   => convert_date(params[:check_in]),
+          'departure' => convert_date(params[:check_out]),
+          'curr'      => THH::Commands::PropertiesFetcher::CURRENCY,
+          'id'        => params[:property_id]
+        }
+      end
+
+      # Converts date string to THH expected format
+      def convert_date(date)
+        Date.strptime(date, ROOMORAMA_DATE_FORMAT).strftime(THH_DATE_FORMAT)
+      end
+    end
+  end
+end
---
+++ lib/concierge/suppliers/thh/price.rb
@@ -0,0 +1,120 @@
+module THH
+
+  # +THH::Price+
+  #
+  # This class is responsible for performing price quotations for properties coming
+  # from THH, parsing the response and building the +Quotation+ object according
+  # with the data returned by their API.
+  #
+  # Usage
+  #
+  #   result = THH::Price.new.quote(stay_params)
+  #   if result.success?
+  #     process_quotation(result.value)
+  #   else
+  #     handle_error(result.error)
+  #   end
+  #
+  # The +quote+ method returns a +Result+ object that, when successful, encapsulates
+  # the resulting +Quotation+ object. Possible errors at this stage are:
+  #
+  # * +unrecognised_response+:  happens when the request was successful, but the format
+  #                             of the response is not compatible to this class' expectations.
+  # * +invalid_property_error+: only properties that require no confirmation and have enabled prices
+  #                             (accessed without Poplidays call center)
+  #                             are supported at this moment.
+  class Price
+
+    attr_reader :credentials
+
+    def initialize(credentials)
+      @credentials = credentials
+    end
+
+    # Checks the price with Poplidays. Used booking/easy endpoint in "EVALUATION"
+    # mode to get the price.
+    #
+    # Properties have also "mandatory services". These need to be accounted
+    # for when calculating the subtotal. For that purpose, an API call to
+    # the property details endpoint is made and that value is extracted.
+    def quote(params)
+      
+      mandatory_services = retrieve_mandatory_services(params[:property_id])
+      return mandatory_services unless mandatory_services.success?
+
+      quote = retrieve_quote(params)
+      return quote if unknown_errors?(quote)
+
+      mapper.build(params, mandatory_services.value, quote)
+    end
+
+    private
+
+    # Some unsuccessful (not 20X) http statuses of quote request
+    # are valid business cases for us and should be handled:
+    #   409 - stay specified in booking is no more available
+    #   400 - bad arrival/departure date
+    def unknown_errors?(quote)
+      !quote.success? && ![:http_status_400, :http_status_409].include?(quote.error.code)
+    end
+
+    def retrieve_quote(params)
+      fetcher = Poplidays::Commands::QuoteFetcher.new(credentials)
+      fetcher.call(params)
+    end
+
+    def retrieve_mandatory_services(property_id)
+      key = ['property', property_id, 'mandatory_services'].join('.')
+      options = { freshness: MANDATORY_SERVICES_FRESHNESS }
+      with_cache(key, options) {
+        fetcher = Poplidays::Commands::LodgingFetcher.new(credentials)
+        result = fetcher.call(property_id)
+        return result unless result.success?
+
+        lodging = result.value
+        if details_validator(lodging).valid?
+          if lodging['mandatoryServicesPrice']
+            Result.new(lodging['mandatoryServicesPrice'])
+          else
+            no_mandatory_services_data_error
+          end
+        else
+          invalid_property_error
+        end
+      }
+    end
+
+    def details_validator(lodging)
+      Poplidays::Validators::PropertyDetailsValidator.new(lodging)
+    end
+
+    def mapper
+      @mapper ||= Poplidays::Mappers::Quote.new
+    end
+
+    def no_mandatory_services_data_error
+      message = "Expected to find the price for mandatory services under the " +
+        "`mandatoryServicesPrice`, but none was found."
+
+      mismatch(message, caller)
+      Result.error(:unrecognised_response, message)
+    end
+
+    def invalid_property_error
+      message = "Property shouldn't be on request only and should have enabled prices"
+      mismatch(message, caller)
+      Result.error(:invalid_property_error, message)
+    end
+
+    def mismatch(message, backtrace)
+      response_mismatch = Concierge::Context::ResponseMismatch.new(
+        message:   message,
+        backtrace: backtrace
+      )
+
+      Concierge.context.augment(response_mismatch)
+    end
+
+  end
+
+end
