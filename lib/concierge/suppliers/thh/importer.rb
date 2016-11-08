8ff7bcc76a2311171d4086ee2fcca9b73ee721f0
K.Kolotyuk <konstantin.kolotyuk@gmail.com>
2016-11-08 14:55:38 +0300

    Fix ruby docs

--- lib/concierge/suppliers/thh/importer.rb
+++ lib/concierge/suppliers/thh/importer.rb
@@ -11,14 +11,14 @@ module THH
     end
 
     # Fetches all properties from THH API
-    # Returns the Result wrapping the array of Hash.
+    # Returns the +Result+ wrapping the +Array+ of +Concierge::SafeAccessHash+.
     def fetch_properties
       fetcher = Commands::PropertiesFetcher.new(credentials)
       fetcher.call
     end
 
     # Fetches property details from THH API
-    # Returns the Result wrapping the SafeAccessHash.
+    # Returns the +Result+ wrapping the +Concierge::SafeAccessHash+.
     def fetch_property(property_id)
       fetcher = Commands::PropertyFetcher.new(credentials)
       fetcher.call(property_id)
