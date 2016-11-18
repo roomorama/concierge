e1f730df5fa45ee85202643a9a258fcdb69d89ef
Keang <skeang@gmail.com>
2016-11-18 16:37:22 +0800

    Missing s

--- spec/lib/concierge/suppliers/waytostay/properties/city_tourist_tax_spec.rb
+++ spec/lib/concierge/suppliers/waytostay/properties/city_tourist_tax_spec.rb
@@ -64,7 +64,7 @@ RSpec.describe Waytostay::Properties::CityTouristTax do
       tax_in_words = described_class.new(taxes, currency).parse
       expect(tax_in_words[:en]).to eq "Additional City Tourist Tax: 13 EUR per night will need to be paid at the property for up to 10 nights.\n"
       expect(tax_in_words[:de]).to eq "Zusätzliche Abgabe für Touristen in Höhe von 13 EUR pro Nacht wird direkt beim Gastgeber bezahlt, für bis zu 10 Nächte.\n"
-      expect(tax_in_words[:es]).to eq "Impuesto municipal de turismo adicional: 13 EUR por noche tendrá que ser pagado en la propieda para un máximo de 10 noche.\n"
+      expect(tax_in_words[:es]).to eq "Impuesto municipal de turismo adicional: 13 EUR por noche tendrá que ser pagado en la propieda para un máximo de 10 noches.\n"
       expect(tax_in_words[:zh]).to eq "附加城市旅游税13 EUR 每晚，需到达酒店时支付最多10晚\n"
       expect(tax_in_words[:zh_tw]).to eq "附加城市旅游税13 EUR 每晚，需到達酒店時支付最多10晚\n"
     end
@@ -110,7 +110,7 @@ RSpec.describe Waytostay::Properties::CityTouristTax do
       tax_in_words = described_class.new(taxes, currency).parse
       expect(tax_in_words[:en]).to eq "Additional City Tourist Tax: 0.83 EUR per night per person will need to be paid at the property for guests above 18 years of age for up to 10 nights.\n"
       expect(tax_in_words[:de]).to eq "Zusätzliche Abgabe für Touristen in Höhe von 0.83 EUR pro Nacht, pro Person wird direkt beim Gastgeber bezahlt, für Gäste älter als 18 Jahre, für bis zu 10 Nächte.\n"
-      expect(tax_in_words[:es]).to eq "Impuesto municipal de turismo adicional: 0.83 EUR por noche tendrá que ser pagado en la propieda para un máximo de 10 noche.\n"
+      expect(tax_in_words[:es]).to eq "Impuesto municipal de turismo adicional: 0.83 EUR por noche tendrá que ser pagado en la propieda para un máximo de 10 noches.\n"
       expect(tax_in_words[:zh]).to eq "超过18岁的客人，附加城市旅游税0.83 EUR每人，每晚需到达酒店时支付，最多10晚。\n"
       expect(tax_in_words[:zh_tw]).to eq "超过18岁的客人，附加城市旅游税0.83 EUR每人，每晚需到達酒店時支付，最多10晚。\n"
     end
