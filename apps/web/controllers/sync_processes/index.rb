861a73b2358a070c9d4b44535b5014ced8370e90
keang <skeang@gmail.com>
2017-01-26 19:08:49 +0800

    Scope sync processes by host

--- apps/web/controllers/sync_processes/index.rb
+++ apps/web/controllers/sync_processes/index.rb
@@ -8,6 +8,7 @@ module Web::Controllers::SyncProcesses
 
     params do
       include Web::Controllers::Params::Paginated
+      param :host_id, type: Integer
     end
 
     expose :metadata_processes, :availabilities_processes
@@ -16,12 +17,16 @@ module Web::Controllers::SyncProcesses
       page = params[:page]
       per  = params[:per]
 
-      @metadata_processes = SyncProcessRepository.
+      relevant_meta_processes = SyncProcessRepository
+      relevant_meta_processes = relevant_meta_processes.for_host(HostRepository.find params[:host_id]) if params[:host_id]
+      @metadata_processes = relevant_meta_processes.
         most_recent.
         paginate(page: page, per: per).
         of_type("metadata")
 
-      @availabilities_processes = SyncProcessRepository.
+      relevant_avail_processes = SyncProcessRepository
+      relevant_avail_processes = relevant_avail_processes.for_host(HostRepository.find params[:host_id]) if params[:host_id]
+      @availabilities_processes = relevant_avail_processes.
         most_recent.
         paginate(page: page, per: per).
         of_type("availabilities")
