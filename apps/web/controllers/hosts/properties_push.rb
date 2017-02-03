dade85a2f443c5462b6c960cb454affdaa498b73
keang <skeang@gmail.com>
2017-02-03 18:25:55 +0800

    Update new class name

--- apps/web/controllers/hosts/properties_push.rb
+++ apps/web/controllers/hosts/properties_push.rb
@@ -2,7 +2,7 @@ module Web::Controllers::Hosts
   class PropertiesPush
     include Web::Action
     def call(params)
-      Concierge::Flows::PropertyPushJobEnqueue.new(property_ids).call
+      Concierge::Flows::PropertiesPushJobEnqueue.new(property_ids).call
       flash[:notice] = "Properties push process queued. Please check External Error for any errors."
       redirect_to routes.supplier_host_path(supplier_id: params[:supplier_id],
                                             id: params[:id])
