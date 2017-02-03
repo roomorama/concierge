17440c054a7a75b3b733917616c12a23523a9372
keang <skeang@gmail.com>
2017-02-03 17:55:52 +0800

    Use the right class and add a flash message

--- apps/web/controllers/hosts/properties_push.rb
+++ apps/web/controllers/hosts/properties_push.rb
@@ -2,12 +2,17 @@ module Web::Controllers::Hosts
   class PropertiesPush
     include Web::Action
     def call(params)
-      Concierge::Flows::PropertyPush.new(host).call
+      Concierge::Flows::PropertyPushJobEnqueue.new(property_ids).call
+      flash[:notice] = "Properties push process queued. Please check External Error for any errors."
       redirect_to routes.supplier_host_path(supplier_id: params[:supplier_id],
                                             id: params[:id])
     end
 
     private
+
+    def property_ids
+      PropertyRepository.from_host(host).collect(&:id)
+    end
 
     def host
       @host ||= HostRepository.find params[:id]
